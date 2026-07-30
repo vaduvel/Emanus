ba7b85e8015f03d031c851ffa842dfeb8bb1bb3f
vaduvel <vaduvadaniel10@yahoo.com>
2026-07-30 21:10:22 +0300

    content(biblia): Geneza 5, verset cu verset

--- packages/shared/src/bible/index.ts
+++ packages/shared/src/bible/index.ts
@@ -5,11 +5,12 @@ import { GENEZA as GENEZA_BASE } from "./geneza.js"
 import { GENEZA_2 } from "./geneza2.js"
 import { GENEZA_3 } from "./geneza3.js"
 import { GENEZA_4 } from "./geneza4.js"
+import { GENEZA_5 } from "./geneza5.js"
 
 /** Geneza, cu toate capitolele scrise pana acum. */
 export const GENEZA: BibleBook = {
   ...GENEZA_BASE,
-  chapters: [...GENEZA_BASE.chapters, GENEZA_2, GENEZA_3, GENEZA_4],
+  chapters: [...GENEZA_BASE.chapters, GENEZA_2, GENEZA_3, GENEZA_4, GENEZA_5],
 }
 
 /** Cartile scrise pana acum. Se adauga pe rand, dupa revizie. */
