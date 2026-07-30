2377fc1c1613c9cb51f5428b6f252afe9c22fd22
vaduvel <vaduvadaniel10@yahoo.com>
2026-07-30 21:40:27 +0300

    content(biblia): Geneza 9, verset cu verset

--- packages/shared/src/bible/index.ts
+++ packages/shared/src/bible/index.ts
@@ -9,6 +9,7 @@ import { GENEZA_5 } from "./geneza5.js"
 import { GENEZA_6 } from "./geneza6.js"
 import { GENEZA_7 } from "./geneza7.js"
 import { GENEZA_8 } from "./geneza8.js"
+import { GENEZA_9 } from "./geneza9.js"
 
 /** Geneza, cu toate capitolele scrise pana acum. */
 export const GENEZA: BibleBook = {
@@ -22,6 +23,7 @@ export const GENEZA: BibleBook = {
     GENEZA_6,
     GENEZA_7,
     GENEZA_8,
+    GENEZA_9,
   ],
 }
 
