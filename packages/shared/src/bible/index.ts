4e5c68bca54ade9b2e91d79edc762281b5985773
vaduvel <vaduvadaniel10@yahoo.com>
2026-07-31 08:46:31 +0300

    content(biblia): Geneza 39, verset cu verset

--- packages/shared/src/bible/index.ts
+++ packages/shared/src/bible/index.ts
@@ -39,6 +39,7 @@ import { GENEZA_35 } from "./geneza35.js"
 import { GENEZA_36 } from "./geneza36.js"
 import { GENEZA_37 } from "./geneza37.js"
 import { GENEZA_38 } from "./geneza38.js"
+import { GENEZA_39 } from "./geneza39.js"
 
 /** Geneza, cu toate capitolele scrise pana acum. */
 export const GENEZA: BibleBook = {
@@ -82,6 +83,7 @@ export const GENEZA: BibleBook = {
     GENEZA_36,
     GENEZA_37,
     GENEZA_38,
+    GENEZA_39,
   ],
 }
 
