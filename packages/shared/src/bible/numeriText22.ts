2c2dd3b4ea96c72ff0fce6c31cbca1a27ad2d10c
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:49:09 +0300

    Numeri 22: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText22.ts
@@ -0,0 +1,53 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 22.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.22.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 41 de versete.
+ */
+
+export const NUMERI_TEXT_22: Record<number, readonly string[]> = {
+  22: [
+    "Fiii lui Israel au pornit \u0219i au t\u0103b\u0103r\u00e2t \u00een c\u00e2mpiile Moabului, dincolo de Iordan, \u00een fa\u021ba Ierihonului.",
+    "Balac, fiul lui \u0162ipor, a v\u0103zut tot ce-i f\u0103cuse Israel lui Amorit.",
+    "\u015ei Moabul s-a sp\u0103im\u00e2ntat foarte tare de popor, pentru c\u0103 era numeros; Moabul a fost cuprins de groaz\u0103 din pricina fiilor lui Israel.",
+    "Moabul a zis b\u0103tr\u00e2nilor Madianului: \u201eAcum adunarea aceasta va \u00eenghi\u021bi tot ce este \u00een jurul nostru, cum \u00eenghi\u021be boul iarba de pe c\u00e2mp.\u201d Balac, fiul lui \u0162ipor, era atunci \u00eempar at al Moabului.",
+    "El a trimis soli la Balaam, fiul lui Beor, la Petor, care este pe R\u00e2u, \u00een \u021bara fiilor poporului s\u0103u, ca s\u0103-l cheme, zic\u00e2nd: \u201eIat\u0103 c\u0103 un popor a ie\u015fit din Egipt; iat\u0103 c\u0103 acoper\u0103 fa\u021ba p\u0103m\u00e2ntului \u0219i a t\u0103b\u0103r\u00e2t \u00een fa\u021ba mea.",
+    "Vino acum, te rog, \u0219i blestem\u0103-mi pe poporul acesta, c\u0103ci este mai puternic dec\u00e2t mine; poate a\u015fa \u00eel voi putea bate \u0219i-l voi izgoni din \u021bar\u0103, c\u0103ci \u015ftiu c\u0103 pe cine binecuv\u00e2ntezi tu este binecuv\u00e2ntat \u0219i pe cine blestemi tu este blestemat.\u201d",
+    "B\u0103tr\u00e2nii Moabului \u0219i b\u0103tr\u00e2nii Madianului au plecat av\u00e2nd \u00een m\u00e2ini plata ghicitului. Au ajuns la Balaam \u0219i i-au spus cuvintele lui Balac.",
+    "El le-a zis: \u201eR\u0103m\u00e2ne\u021bi aici peste noapte \u0219i v\u0103 voi aduce r\u0103spuns cum \u00eemi va vorbi DOMNUL.\u201d \u015ei frunta\u015fii Moabului au r\u0103mas la Balaam.",
+    "Dumnezeu a venit la Balaam \u0219i a zis: \u201eCine sunt oamenii ace\u015ftia care sunt la tine?\u201d",
+    "Balaam I-a r\u0103spuns lui Dumnezeu: \u201eBalac, fiul lui \u0162ipor, \u00eempar atul Moabului, i-a trimis la mine, zic\u00e2nd:",
+    "\u00abIat\u0103 c\u0103 poporul care a ie\u015fit din Egipt acoper\u0103 fa\u021ba p\u0103m\u00e2ntului; vino acum \u0219i blestem\u0103-mi-l; poate a\u015fa m\u0103 voi putea lupta \u00eempotriva lui \u0219i-l voi izgoni.\u00bb\u201d",
+    "Dumnezeu i-a spus lui Balaam: \u201eS\u0103 nu te duci cu ei; s\u0103 nu blestemi poporul acela, c\u0103ci este binecuv\u00e2ntat!\u201d",
+    "Balaam s-a sculat diminea\u021ba \u0219i a zis frunta\u015filor lui Balac: \u201eDuce\u021bi-v\u0103 \u00een \u021bara voastr\u0103, c\u0103ci DOMNUL nu vrea s\u0103 m\u0103 lase s\u0103 merg cu voi.\u201d",
+    "Frunta\u015fii Moabului s-au sculat, s-au \u00eentors la Balac \u0219i au zis: \u201eBalaam a refuzat s\u0103 vin\u0103 cu noi.\u201d",
+    "Balac a trimis din nou frunta\u015fi, mai mul\u021bi \u0219i mai cu vaz\u0103 dec\u00e2t primii.",
+    "Ei au ajuns la Balaam \u0219i i-au zis: \u201eA\u015fa zice Balac, fiul lui \u0162ipor: \u00abNu te l\u0103sa oprit de la venirea la mine;",
+    "c\u0103ci \u00ee\u021bi voi da mari cinste \u0219i voi face tot ce-mi vei spune; vino, te rog, \u0219i blestem\u0103-mi pe poporul acesta!\u00bb\u201d",
+    "Balaam a r\u0103spuns \u0219i a zis slujitorilor lui Balac: \u201eDac\u0103 mi-ar da Balac casa lui plin\u0103 cu argint \u0219i aur, tot nu a\u015f putea s\u0103 calc porunca DOMNULUI, Dumnezeul meu, ca s\u0103 fac un lucru mic sau mare.",
+    "Acum, v\u0103 rog, r\u0103m\u00e2ne\u021bi \u0219i voi aici peste noapte, ca s\u0103 aflu ce-mi va mai spune DOMNUL.\u201d",
+    "Dumnezeu a venit noaptea la Balaam \u0219i i-a zis: \u201eDac\u0103 oamenii ace\u015ftia au venit s\u0103 te cheme, scoal\u0103-te \u0219i du-te cu ei; dar s\u0103 faci numai ce-\u021bi voi spune!\u201d",
+    "Balaam s-a sculat diminea\u021ba, a pus \u015faua pe m\u0103g\u0103ri\u021ba lui \u0219i a plecat cu frunta\u015fii Moabului.",
+    "M\u00e2nia lui Dumnezeu S-a aprins pentru c\u0103 plecase; \u0219i \u00cengeru l DOMNULUI S-a a\u015fezat pe drum ca s\u0103 i Se \u00eempotriveasc\u0103. Balaam era c\u0103lare pe m\u0103g\u0103ri\u021ba lui \u0219i cei doi slujitori ai lui erau cu el.",
+    "M\u0103g\u0103ri\u021ba L-a v\u0103zut pe \u00cengeru l DOMNULUI st\u00e2nd pe drum cu sabia scoas\u0103 \u00een m\u00e2n\u0103; m\u0103g\u0103ri\u021ba s-a ab\u0103tut de pe drum \u0219i a luat-o pe c\u00e2mp. Balaam a b\u0103tut m\u0103g\u0103ri\u021ba ca s\u0103 o \u00eentoarc\u0103 pe drum.",
+    "\u00cengeru l DOMNULUI S-a a\u015fezat \u00eentr-o potec\u0103 str\u00e2mt\u0103 dintre vii, av\u00e2nd un zid \u00eentr-o parte \u0219i un zid \u00een cealalt\u0103 parte.",
+    "M\u0103g\u0103ri\u021ba L-a v\u0103zut pe \u00cengeru l DOMNULUI, s-a lipit de zid \u0219i a str\u00e2ns piciorul lui Balaam de zid. El a b\u0103tut-o din nou.",
+    "\u00cengeru l DOMNULUI a mers mai departe \u0219i S-a a\u015fezat \u00eentr-un loc str\u00e2mt unde nu era loc de abatere nici la dreapta, nici la st\u00e2nga.",
+    "M\u0103g\u0103ri\u021ba L-a v\u0103zut pe \u00cengeru l DOMNULUI \u0219i s-a culcat sub Balaam. M\u00e2nia lui Balaam s-a aprins \u0219i a b\u0103tut m\u0103g\u0103ri\u021ba cu toiagul.",
+    "Atunci DOMNUL a deschis gura m\u0103g\u0103ri\u021bei \u0219i ea i-a zis lui Balaam: \u201eCe \u021bi-am f\u0103cut de m-ai b\u0103tut de trei ori?\u201d",
+    "Balaam i-a zis m\u0103g\u0103ri\u021bei: \u201ePentru c\u0103 \u021bi-ai b\u0103tut joc de mine! Dac\u0103 a\u015f fi avut o sabie \u00een m\u00e2n\u0103, te-a\u015f fi ucis acum!\u201d",
+    "M\u0103g\u0103ri\u021ba i-a zis lui Balaam: \u201eNu sunt eu oare m\u0103g\u0103ri\u021ba ta pe care ai c\u0103l\u0103rit din totdeauna p\u00e2n\u0103 \u00een ziua de azi? Am avut eu vreodat\u0103 obiceiul s\u0103-\u021bi fac a\u015fa?\u201d El a r\u0103spuns: \u201eNu.\u201d",
+    "Atunci DOMNUL a deschis ochii lui Balaam \u0219i el L-a v\u0103zut pe \u00cengeru l DOMNULUI st\u00e2nd pe drum cu sabia scoas\u0103 \u00een m\u00e2n\u0103. El s-a plecat \u0219i s-a aruncat cu fa\u021ba la p\u0103m\u00e2nt.",
+    "\u00cengeru l DOMNULUI i-a zis: \u201eDe ce \u021bi-ai b\u0103tut m\u0103g\u0103ri\u021ba de trei ori? Iat\u0103 c\u0103 Eu am ie\u015fit ca s\u0103-\u021bi stau \u00eempotriva, pentru c\u0103 drumul t\u0103u este un drum al pierz\u0103rii \u00eenaintea Mea.",
+    "M\u0103g\u0103ri\u021ba M-a v\u0103zut \u0219i s-a ab\u0103tut dinaintea Mea de trei ori. Dac\u0103 nu s-ar fi ab\u0103tut dinaintea Mea, te-a\u015f fi ucis pe tine acum, iar pe ea a\u015f fi l\u0103sat-o vie!\u201d",
+    "Balaam I-a zis \u00cengeru lui DOMNULUI: \u201eAm p\u0103c\u0103tuit, c\u0103ci nu \u015ftiam c\u0103 Tu stai \u00eenaintea mea pe drum. Acum, dac\u0103 nu-\u0162i place, m\u0103 voi \u00eentoarce.\u201d",
+    "\u00cengeru l DOMNULUI i-a spus lui Balaam: \u201eDu-te cu b\u0103rba\u021bii ace\u015ftia; dar s\u0103 roste\u015fti numai cuv\u00e2ntul pe care \u021bi-l voi spune Eu!\u201d \u015ei Balaam a plecat cu frunta\u015fii lui Balac.",
+    "C\u00e2nd a auzit Balac c\u0103 vine Balaam, i-a ie\u015fit \u00een \u00eent\u00e2mpinare la cetatea Moabului, care este la hotarul Arnonului, la marginea cea mai \u00eendep\u0103rtat\u0103 a hotarului.",
+    "Balac i-a zis lui Balaam: \u201eNu am trimis eu oare soli la tine ca s\u0103 te cheme? De ce nu ai venit la mine? Oare nu pot eu s\u0103-\u021bi dau cinste?\u201d",
+    "Balaam i-a r\u0103spuns lui Balac: \u201eIat\u0103 c\u0103 am venit la tine! Dar pot eu oare s\u0103 spun ceva de la mine? Cuv\u00e2ntul pe care-l va pune Dumnezeu \u00een gura mea, pe acela \u00eel voi rosti.\u201d",
+    "Balaam a mers cu Balac \u0219i au ajuns la Chiriat-Hu\u021bot.",
+    "Balac a jertfit boi \u0219i oi \u0219i i-a trimis lui Balaam \u0219i frunta\u015filor care erau cu el.",
+    "A doua zi diminea\u021ba, Balac l-a luat pe Balaam \u0219i l-a suit la Bamot-Baal, de unde putea vedea marginea poporului.",
+  ],
+}
