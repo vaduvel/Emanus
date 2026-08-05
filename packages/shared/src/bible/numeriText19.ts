86efb37d8f8cdde0457fd30f82d4e54f5085e881
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:44:19 +0300

    Numeri 19: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText19.ts
@@ -0,0 +1,34 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 19.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.19.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 22 de versete.
+ */
+
+export const NUMERI_TEXT_19: Record<number, readonly string[]> = {
+  19: [
+    "DOMNUL le-a vorbit lui Moise \u0219i lui Aaron, spun\u00e2nd:",
+    "\u201eAceasta este or\u00e2nduirea legii pe care a poruncit-o DOMNUL, zic\u00e2nd: Spune-le fiilor lui Israel s\u0103-\u021bi aduc\u0103 o vac\u0103 ro\u015fie f\u0103r\u0103 cusur, care s\u0103 nu aib\u0103 niciun defect \u0219i peste care s\u0103 nu fi fost pus niciodat\u0103 jugul.",
+    "S\u0103 o da\u021bi preotului Eleazar; el s\u0103 o scoat\u0103 afar\u0103 din tab\u0103r\u0103 \u0219i s\u0103 fie \u00eenjunghiat\u0103 \u00eenaintea lui.",
+    "Preotul Eleazar s\u0103 ia cu degetul din s\u00e2ngele ei \u0219i s\u0103 stropeasc\u0103 cu s\u00e2ngele ei de \u0219apte ori spre partea din fa\u021b\u0103 a Cortului \u00cent\u00e2lnirii.",
+    "Vaca s\u0103 fie ars\u0103 sub ochii lui: pielea ei, carnea ei, s\u00e2ngele ei \u00eempreun\u0103 cu baliga ei s\u0103 fie arse.",
+    "Preotul s\u0103 ia lemn de cedru, isop \u0219i stacojiu \u0219i s\u0103 le arunce \u00een mijlocul focului \u00een care arde vaca.",
+    "Apoi preotul s\u0103-\u0219i spele hainele \u0219i s\u0103-\u0219i scalde trupul \u00een ap\u0103; dup\u0103 aceea va putea intra \u00een tab\u0103r\u0103, dar va fi necurat p\u00e2n\u0103 seara.",
+    "Cel ce a ars-o s\u0103-\u0219i spele hainele \u00een ap\u0103, s\u0103-\u0219i scalde trupul \u00een ap\u0103 \u0219i va fi necurat p\u00e2n\u0103 seara.",
+    "Un om curat s\u0103 str\u00e2ng\u0103 cenu\u015fa vacii \u0219i s\u0103 o pun\u0103 afar\u0103 din tab\u0103r\u0103 \u00eentr-un loc curat; ea s\u0103 fie p\u0103strat\u0103 pentru adunarea fiilor lui Israel ca ap\u0103 de cur\u0103\u021bire: este o jertf\u0103 pentru p\u0103cat.",
+    "Cel ce va str\u00e2nge cenu\u015fa vacii s\u0103-\u0219i spele hainele \u0219i va fi necurat p\u00e2n\u0103 seara. Aceasta va fi o or\u00e2nduial\u0103 ve\u015fnic\u0103 pentru fiii lui Israel \u0219i pentru str\u0103inul care locuie\u015fte printre ei.",
+    "Cel ce se va atinge de vreun trup mort de om va fi necurat \u0219apte zile.",
+    "S\u0103 se cure\u021beasc\u0103 cu apa aceasta \u00een ziua a treia \u0219i \u00een ziua a \u0219aptea \u0219i va fi curat; dar dac\u0103 nu se va cur\u0103\u021bi \u00een ziua a treia \u0219i \u00een ziua a \u0219aptea, nu va fi curat.",
+    "Oricine se atinge de un trup mort, de trupul unui om care a murit, \u0219i nu se cur\u0103\u021be\u015fte, spurc\u0103 Cortul DOMNULUI; omul acela va fi nimicit din Israel. Pentru c\u0103 apa de cur\u0103\u021bire nu a fost stropit\u0103 peste el, este necurat; necur\u0103\u021bia lui r\u0103m\u00e2ne asupra lui.",
+    "Aceasta este legea c\u00e2nd un om moare \u00eentr-un cort: oricine intr\u0103 \u00een cort \u0219i tot ce este \u00een cort va fi necurat \u0219apte zile.",
+    "Orice vas deschis care nu are un capac legat peste el va fi necurat.",
+    "Oricine se va atinge pe c\u00e2mp de un om ucis de sabie sau de un mort, sau de un os de om sau de un morm\u00e2nt va fi necurat \u0219apte zile.",
+    "Pentru cel necurat s\u0103 se ia din cenu\u015fa arderii jertfei pentru p\u0103cat \u0219i s\u0103 se toarne peste ea ap\u0103 curg\u0103toare \u00eentr-un vas.",
+    "Un om curat s\u0103 ia isop, s\u0103-l moaie \u00een ap\u0103 \u0219i s\u0103 stropeasc\u0103 cortul, toate uneltele, persoanele care erau acolo \u0219i pe cel ce s-a atins de un os, de un ucis, de un mort sau de un morm\u00e2nt.",
+    "Omul cel curat s\u0103-l stropeasc\u0103 pe cel necurat \u00een ziua a treia \u0219i \u00een ziua a \u0219aptea; \u0219i \u00een ziua a \u0219aptea s\u0103-l cure\u021beasc\u0103. Cel cur\u0103\u021bit s\u0103-\u0219i spele hainele, s\u0103 se scalde \u00een ap\u0103 \u0219i seara va fi curat.",
+    "Dar omul care va fi necurat \u0219i nu se va cur\u0103\u021bi, sufletul aceluia va fi nimicit din mijlocul adun\u0103rii, pentru c\u0103 a spurcat Sf\u00e2ntul L\u0103ca\u015f al DOMNULUI; apa de cur\u0103\u021bire nu a fost stropit\u0103 peste el: este necurat.",
+    "Aceasta va fi o or\u00e2nduial\u0103 ve\u015fnic\u0103 pentru ei. Cel ce va stropi apa de cur\u0103\u021bire s\u0103-\u0219i spele hainele; iar cel ce se va atinge de apa de cur\u0103\u021bire va fi necurat p\u00e2n\u0103 seara.",
+    "Tot ce va atinge cel necurat va fi necurat; iar persoana care se va atinge de el va fi necurat\u0103 p\u00e2n\u0103 seara.\u201d",
+  ],
+}
