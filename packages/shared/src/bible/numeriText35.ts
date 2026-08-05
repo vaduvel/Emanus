9c3c4f4c288a59f4b8cd399a7c44d4a8dbb46295
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 16:05:59 +0300

    Numeri 35: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText35.ts
@@ -0,0 +1,46 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 35.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.35.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 34 de versete.
+ */
+
+export const NUMERI_TEXT_35: Record<number, readonly string[]> = {
+  35: [
+    "DOMNUL i-a vorbit lui Moise \u00een c\u00e2mpiile Moabului, l\u00e2ng\u0103 Iordan, \u00een fa\u021ba Ierihonului, spun\u00e2nd:",
+    "\u201ePorunce\u015fte-le fiilor lui Israel s\u0103 le dea levi\u021bilor, din mo\u015ftenirea st\u0103p\u00e2nirii lor, cet\u0103\u021bi \u00een care s\u0103 locuiasc\u0103; \u0219i s\u0103 le da\u021bi levi\u021bilor \u0219i p\u0103\u015funile din jurul cet\u0103\u021bilor.",
+    "Cet\u0103\u021bile vor fi ale lor ca s\u0103 locuiasc\u0103 \u00een ele, iar p\u0103\u015funile lor vor fi pentru vitele lor, pentru averile lor \u0219i pentru toate animalele lor.",
+    "P\u0103\u015funile cet\u0103\u021bilor pe care le ve\u021bi da levi\u021bilor se vor \u00eentinde de la zidul cet\u0103\u021bii \u00een afar\u0103 o mie de co\u021bi de jur \u00eemprejur.",
+    "S\u0103 m\u0103sura\u021bi afar\u0103 din cetate dou\u0103 mii de co\u021bi pe partea de r\u0103s\u0103rit, dou\u0103 mii de co\u021bi pe partea de sud, dou\u0103 mii de co\u021bi pe partea de apus \u0219i dou\u0103 mii de co\u021bi pe partea de nord, iar cetatea va fi \u00een mijloc. Acestea vor fi p\u0103\u015funile cet\u0103\u021bilor lor.",
+    "Dintre cet\u0103\u021bile pe care le ve\u021bi da levi\u021bilor, \u015fase vor fi cet\u0103\u021bi de sc\u0103pare pe care le ve\u021bi da ca s\u0103 poat\u0103 fugi acolo uciga\u015ful; pe l\u00e2ng\u0103 ele le ve\u021bi da patruzeci \u0219i dou\u0103 de cet\u0103\u021bi.",
+    "Toate cet\u0103\u021bile pe care le ve\u021bi da levi\u021bilor vor fi patruzeci \u0219i opt de cet\u0103\u021bi, \u00eempreun\u0103 cu p\u0103\u015funile lor.",
+    "Cet\u0103\u021bile pe care le ve\u021bi da din st\u0103p\u00e2nirea fiilor lui Israel s\u0103 le da\u021bi mai multe de la cei mai numero\u015fi \u0219i mai pu\u021bine de la cei mai pu\u021bini; fiecare va da din cet\u0103\u021bile sale levi\u021bilor dup\u0103 mo\u015ftenirea pe care o va primi.\u201d",
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eSpune-le fiilor lui Israel \u0219i vorbe\u015fte-le: C\u00e2nd ve\u021bi trece Iordanul \u00een \u021bara Canaanului,",
+    "s\u0103 v\u0103 alege\u021bi cet\u0103\u021bi care s\u0103 v\u0103 fie cet\u0103\u021bi de sc\u0103pare, unde s\u0103 poat\u0103 fugi uciga\u015ful care a ucis pe cineva din gre\u015feal\u0103.",
+    "Cet\u0103\u021bile v\u0103 vor sluji ca sc\u0103pare \u00eempotriva r\u0103zbun\u0103torului s\u00e2ngelui, ca uciga\u015ful s\u0103 nu moar\u0103 \u00eenainte de a se \u00eenf\u0103\u021bi\u015fa la judecat\u0103 \u00eenaintea adun\u0103rii.",
+    "Dintre cet\u0103\u021bile pe care le ve\u021bi da, \u015fase s\u0103 fie cet\u0103\u021bi de sc\u0103pare:",
+    "trei cet\u0103\u021bi s\u0103 da\u021bi dincolo de Iordan \u0219i trei cet\u0103\u021bi s\u0103 da\u021bi \u00een \u021bara Canaanului; ele vor fi cet\u0103\u021bi de sc\u0103pare.",
+    "Aceste \u015fase cet\u0103\u021bi vor sluji ca sc\u0103pare pentru fiii lui Israel, pentru str\u0103in \u0219i pentru cel ce locuie\u015fte printre ei, ca s\u0103 poat\u0103 fugi acolo oricine a ucis pe cineva din gre\u015feal\u0103.",
+    "Dac\u0103 cineva \u00eel love\u015fte pe altul cu o unealt\u0103 de fier \u0219i acesta moare, este un uciga\u015f; uciga\u015ful s\u0103 fie dat la moarte.",
+    "Dac\u0103 \u00eel love\u015fte cu o piatr\u0103 din m\u00e2n\u0103 de la care se poate muri, \u0219i acesta moare, este un uciga\u015f; uciga\u015ful s\u0103 fie dat la moarte.",
+    "Sau dac\u0103 \u00eel love\u015fte cu o unealt\u0103 de lemn din m\u00e2n\u0103 de la care se poate muri, \u0219i acesta moare, este un uciga\u015f; uciga\u015ful s\u0103 fie dat la moarte.",
+    "R\u0103zbun\u0103torul s\u00e2ngelui \u00eel va omor\u00ee pe uciga\u015f; c\u00e2nd \u00eel va \u00eent\u00e2lni, \u00eel va omor\u00ee.",
+    "Dac\u0103 cineva \u00eel \u00eempinge pe altul din ur\u0103 sau arunc\u0103 ceva asupra lui la p\u00e2nd\u0103 \u0219i acesta moare,",
+    "sau dac\u0103 \u00eel love\u015fte din du\u015fm\u0103nie cu m\u00e2na \u0219i acesta moare, cel ce a lovit s\u0103 fie dat la moarte: este un uciga\u015f; r\u0103zbun\u0103torul s\u00e2ngelui \u00eel va omor\u00ee pe uciga\u015f c\u00e2nd \u00eel va \u00eent\u00e2lni.",
+    "Dar dac\u0103 \u00eel \u00eempinge f\u0103r\u0103 du\u015fm\u0103nie sau arunc\u0103 ceva asupra lui f\u0103r\u0103 s\u0103 fi stat la p\u00e2nd\u0103,",
+    "sau dac\u0103 arunc\u0103 o piatr\u0103 de la care se poate muri, f\u0103r\u0103 s\u0103-l vad\u0103, \u0219i aceea cade peste el \u0219i moare, de\u015fi nu-i era vr\u0103jma\u015f \u0219i nu-i c\u0103uta r\u0103ul,",
+    "atunci adunarea s\u0103 judece \u00eentre cel ce a lovit \u0219i r\u0103zbun\u0103torul s\u00e2ngelui, dup\u0103 aceste legi.",
+    "Adunarea s\u0103-l izb\u0103veasc\u0103 pe uciga\u015f din m\u00e2na r\u0103zbun\u0103torului s\u00e2ngelui \u0219i adunarea s\u0103-l \u00eentoarc\u0103 \u00een cetatea lui de sc\u0103pare unde fugise; \u0219i s\u0103 r\u0103m\u00e2n\u0103 acolo p\u00e2n\u0103 la moartea marelui preot care a fost uns cu uleiul cel sf\u00e2nt.",
+    "Dar dac\u0103 uciga\u015ful va ie\u015fi din hotarul cet\u0103\u021bii lui de sc\u0103pare unde fugise,",
+    "\u0219i r\u0103zbun\u0103torul s\u00e2ngelui \u00eel va g\u0103si afar\u0103 din hotarul cet\u0103\u021bii lui de sc\u0103pare, \u0219i r\u0103zbun\u0103torul s\u00e2ngelui \u00eel va ucide pe uciga\u015f, nu va fi vinovat de s\u00e2nge;",
+    "c\u0103ci uciga\u015ful trebuia s\u0103 r\u0103m\u00e2n\u0103 \u00een cetatea lui de sc\u0103pare p\u00e2n\u0103 la moartea marelui preot; dar dup\u0103 moartea marelui preot, uciga\u015ful se va putea \u00eentoarce \u00een \u021bara mo\u015ftenirii sale.",
+    "Aceste lucruri s\u0103 v\u0103 fie o or\u00e2nduial\u0103 de drept \u00een neamurile voastre, \u00een toate locuin\u021bele voastre.",
+    "Dac\u0103 cineva ucide un om, uciga\u015ful s\u0103 fie omor\u00e2t pe vorba martorilor; dar un singur martor nu va putea depune m\u0103rturie \u00eempotriva unei persoane ca s\u0103 fie condamnat\u0103 la moarte.",
+    "S\u0103 nu primi\u021bi r\u0103scump\u0103rare pentru via\u021ba unui uciga\u015f vinovat de moarte, ci el s\u0103 fie dat la moarte.",
+    "S\u0103 nu primi\u021bi r\u0103scump\u0103rare de la cel ce a fugit \u00een cetatea lui de sc\u0103pare ca s\u0103 se \u00eentoarc\u0103 s\u0103 locuiasc\u0103 \u00een \u021bar\u0103 \u00eenainte de moartea preotului.",
+    "S\u0103 nu spurca\u021bi \u021bara \u00een care sunte\u021bi, c\u0103ci s\u00e2ngele spurc\u0103 \u021bara; \u0219i nu se poate face isp\u0103\u015fire pentru \u021bar\u0103 din pricina s\u00e2ngelui v\u0103rsat \u00een ea dec\u00e2t prin s\u00e2ngele celui ce l-a v\u0103rsat.",
+    "S\u0103 nu necinsti\u021bi \u021bara \u00een care locui\u021bi \u0219i \u00een mijlocul c\u0103reia locuiesc Eu; c\u0103ci Eu sunt DOMNUL care locuiesc \u00een mijlocul fiilor lui Israel.\u201d",
+  ],
+}
