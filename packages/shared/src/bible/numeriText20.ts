0ef8068647c96fdea9fc5bd2e4ad74cdaf9ba3d5
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:45:38 +0300

    Numeri 20: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText20.ts
@@ -0,0 +1,41 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 20.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.20.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 29 de versete.
+ */
+
+export const NUMERI_TEXT_20: Record<number, readonly string[]> = {
+  20: [
+    "Fiii lui Israel, toat\u0103 adunarea, au ajuns \u00een pustia \u0162in \u00een luna \u00eent\u00e2i; \u0219i poporul a r\u0103mas la Cade\u015f. Acolo a murit Maria \u0219i a fost \u00eengropat\u0103 acolo.",
+    "Nu era ap\u0103 pentru adunare; \u0219i s-au str\u00e2ns \u00eempotriva lui Moise \u0219i \u00eempotriva lui Aaron.",
+    "Poporul s-a certat cu Moise \u0219i a zis: \u201eO, de am fi murit c\u00e2nd au murit fra\u021bii no\u015ftri \u00eenaintea DOMNULUI!",
+    "De ce a\u021bi adus adunarea DOMNULUI \u00een pustia aceasta, ca s\u0103 murim aici, noi \u0219i vitele noastre?",
+    "De ce ne-a\u021bi scos din Egipt ca s\u0103 ne aduce\u021bi \u00een acest loc r\u0103u? Nu este un loc de sem\u0103nat, nici de smochini, nici de vi\u021b\u0103, nici de rodii; \u0219i nu este nici ap\u0103 de b\u0103ut!\u201d",
+    "Moise \u0219i Aaron au plecat dinaintea adun\u0103rii la intrarea Cortului \u00cent\u00e2lnirii \u0219i au c\u0103zut cu fa\u021ba la p\u0103m\u00e2nt. \u015ei slava DOMNULUI S-a ar\u0103tat peste ei.",
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eIa toiagul \u0219i str\u00e2nge adunarea, tu \u0219i fratele t\u0103u Aaron. Vorbi\u021bi st\u00e2ncii sub ochii lor \u0219i ea \u00ee\u0219i va da apele. Le vei scoate ap\u0103 din st\u00e2nc\u0103 \u0219i vei ad\u0103pa adunarea \u0219i vitele lor.\u201d",
+    "Moise a luat toiagul dinaintea DOMNULUI, cum \u00eei poruncise.",
+    "Moise \u0219i Aaron au str\u00e2ns adunarea \u00eenaintea st\u00e2ncii. Moise le-a zis: \u201eAsculta\u021bi, r\u0103zvr\u0103ti\u021bilor! Vom putea noi oare s\u0103 v\u0103 scoatem ap\u0103 din st\u00e2nca aceasta?\u201d",
+    "Moise a ridicat m\u00e2na \u0219i a lovit st\u00e2nca de dou\u0103 ori cu toiagul s\u0103u. A ie\u015fit ap\u0103 din bel\u015fug, a\u015fa \u00eenc\u00e2t au b\u0103ut adunarea \u0219i vitele lor.",
+    "Dar DOMNUL le-a spus lui Moise \u0219i lui Aaron: \u201ePentru c\u0103 nu a\u021bi avut credin\u021b\u0103 \u00een Mine ca s\u0103 M\u0103 sfin\u021bi\u021bi \u00een ochii fiilor lui Israel, de aceea nu ve\u021bi duce aceast\u0103 adunare \u00een \u021bara pe care le-o dau!\u201d",
+    "Acestea sunt apele Meriba, unde fiii lui Israel s-au certat cu DOMNUL \u0219i unde El a fost sfin\u021bit printre ei.",
+    "Moise a trimis soli din Cade\u015f la \u00eemparatul Edomului: \u201eA\u015fa zice fratele t\u0103u Israel: Tu \u015ftii toate necazurile prin care am trecut.",
+    "P\u0103rin\u021bii no\u015ftri s-au cobor\u00e2t \u00een Egipt \u0219i am locuit mult\u0103 vreme \u00een Egipt; dar egiptenii ne-au chinuit pe noi \u0219i pe p\u0103rin\u021bii no\u015ftri.",
+    "Am strigat c\u0103tre DOMNUL \u0219i El ne-a auzit glasul, a trimis un \u00cenger \u0219i ne-a scos din Egipt. Iat\u0103 c\u0103 suntem la Cade\u015f, cetate aflat\u0103 la marginea hotarului t\u0103u.",
+    "Las\u0103-ne, te rug\u0103m, s\u0103 trecem prin \u021bara ta; nu vom trece prin ogoare, nici prin vii, \u0219i nu vom bea ap\u0103 din f\u00e2nt\u00e2ni. Vom merge pe Drumul \u00cep\u0103r\u0103tesc f\u0103r\u0103 s\u0103 ne abat em la dreapta sau la st\u00e2nga p\u00e2n\u0103 vom trece de hotarul t\u0103u.\u201d",
+    "Dar Edomul i-a r\u0103spuns: \u201eNu vei trece pe la mine, altfel \u00ee\u021bi voi ie\u015fi \u00een \u00eent\u00e2mpinare cu sabia!\u201d",
+    "Fiii lui Israel i-au zis: \u201eVom merge pe drumul cel mare; iar dac\u0103 vom bea din apa ta, eu \u0219i vitele mele, \u00ee\u021bi voi pl\u0103ti pre\u021bul; nu cer altceva dec\u00e2t s\u0103 trec cu picioarele.\u201d",
+    "Dar el a r\u0103spuns: \u201eNu vei trece!\u201d \u015ei Edomul i-a ie\u015fit \u00een \u00eent\u00e2mpinare cu un popor numeros \u0219i cu m\u00e2n\u0103 puternic\u0103.",
+    "Astfel Edomul a refuzat s\u0103-l lase pe Israel s\u0103 treac\u0103 prin hotarul lui; \u0219i Israel s-a ab\u0103tut de la el.",
+    "Fiii lui Israel, toat\u0103 adunarea, au pornit din Cade\u015f \u0219i au ajuns la muntele Hor.",
+    "DOMNUL le-a vorbit lui Moise \u0219i lui Aaron la muntele Hor, la hotarul \u021b\u0103rii Edomului, spun\u00e2nd:",
+    "\u201eAaron va fi ad\u0103ugat la poporul s\u0103u, c\u0103ci nu va intra \u00een \u021bara pe care am dat-o fiilor lui Israel, pentru c\u0103 v-a\u021bi r\u0103zvr\u0103tit \u00eempotriva cuv\u00e2ntului Meu la apele Meriba.",
+    "Ia-l pe Aaron \u0219i pe fiul s\u0103u Eleazar \u0219i suie-i pe muntele Hor.",
+    "Dezbrac\u0103-l pe Aaron de hainele lui \u0219i \u00eembrac\u0103-l cu ele pe fiul s\u0103u Eleazar; c\u0103ci Aaron va fi ad\u0103ugat acolo \u0219i va muri.\u201d",
+    "Moise a f\u0103cut cum \u00eei poruncise DOMNUL: s-au suit pe muntele Hor \u00een sub ochii \u00eentregii adun\u0103ri.",
+    "Moise l-a dezbrcat pe Aaron de hainele lui \u0219i l-a \u00eembr\u0103cat cu ele pe fiul s\u0103u Eleazar. Aaron a murit acolo pe v\u00e2rful muntelui. Apoi Moise \u0219i Eleazar s-au cobor\u00e2t de pe munte.",
+    "C\u00e2nd toat\u0103 adunarea a v\u0103zut c\u0103 Aaron murise, toat\u0103 casa lui Israel l-a pl\u00e2ns pe Aaron timp de treizeci de zile.",
+  ],
+}
