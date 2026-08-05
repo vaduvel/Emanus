8fdf1aad05f2628c617b6f53093f4f38c635191e
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 09:30:25 +0300

    Exod 23: textul biblic din Biblia Emanus

--- packages/shared/src/bible/exodText23.ts
+++ packages/shared/src/bible/exodText23.ts
@@ -1,15 +1,45 @@
 /*
- * Textul biblic al cărții Exod, capitolul 23.
+ * Textul biblic al c\u0103r\u021bii Exod, capitolul 23.
  *
- * ATENȚIE, agentul care lucrează la Biblia Emanus:
- * fișierul acesta ține LOCUL textului, nu textul. Fiecare intrare din tablou
- * este un verset, în ordine, de la 1 la 33. Înlocuiește fiecare șir cu versetul
- * din Biblia Emanus (validată din trei părți) și nu schimba nimic altceva:
- * nici numărul intrărilor, nici cheia capitolului, nici numele exportului.
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/EXO.23.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 33 de versete.
  */
 
-const PLACEHOLDER = "[textul biblic se completează din Biblia Emanus]"
-
 export const EXOD_TEXT_23: Record<number, readonly string[]> = {
-  23: Array.from({ length: 33 }, (_, i) => `[Exod 23:${i + 1}] ${PLACEHOLDER}`),
+  23: [
+    "\u201eS\u0103 nu r\u0103sp\u00e2nde\u0219ti un zvon fals. S\u0103 nu-\u021bi une\u0219ti m\u00e2na cu cel r\u0103u ca s\u0103 fii martor r\u0103uvoitor.",
+    "S\u0103 nu urmezi mul\u021bimea ca s\u0103 faci r\u0103u \u0219i s\u0103 nu depui m\u0103rturie \u00eentr-un proces \u00eenclin\u00e2nd dup\u0103 majoritate ca s\u0103 pervertesti dreptatea.",
+    "S\u0103 nu-l favorizezi nici pe s\u0103rac \u00een procesul lui.",
+    "Dac\u0103 \u00eent\u00e2lne\u0219ti boul sau m\u0103garul du\u0219manului t\u0103u r\u0103t\u0103cind, s\u0103 i-l aduci \u00eenapoi.",
+    "Dac\u0103 vezi m\u0103garul celui care te ur\u0103\u0219te c\u0103zut sub povara lui, s\u0103 nu-l la\u0219i acolo, ci s\u0103-l aju\u021bi s\u0103-l elibereze.",
+    "S\u0103 nu denaturezi dreptatea s\u0103racului t\u0103u \u00een procesul lui.",
+    "S\u0103 te \u021bii departe de o acuza\u021bie fals\u0103. S\u0103 nu-l condamni la moarte pe cel nevinovat \u0219i drept, c\u0103ci Eu nu-l voi declara drept pe cel vinovat.",
+    "S\u0103 nu prime\u0219ti mit\u0103, c\u0103ci mita \u00eei orbe\u0219te pe cei care v\u0103d limpede \u0219i r\u0103stoarn\u0103 cuvintele celor drep\u021bi.",
+    "S\u0103 nu asupre\u0219ti str\u0103inul. Voi cunoa\u0219te\u021bi sufletul str\u0103inului, c\u0103ci a\u021bi fost str\u0103ini \u00een \u021bara Egiptului.",
+    "\u0218ase ani s\u0103-\u021bi semeni p\u0103m\u00e2ntul \u0219i s\u0103-i str\u00e2ngi recolta,",
+    "dar \u00een anul al \u0219aptelea s\u0103-l la\u0219i s\u0103 se odihneasc\u0103 \u0219i s\u0103 r\u0103m\u00e2n\u0103 nelucrat. S\u0103racii poporului t\u0103u s\u0103 m\u0103n\u00e2nce din el, iar ce r\u0103m\u00e2ne s\u0103 m\u0103n\u00e2nce animalele c\u00e2mpului. La fel s\u0103 faci cu via \u0219i cu livada ta de m\u0103slini.",
+    "\u0218ase zile s\u0103-\u021bi faci lucrarea, iar \u00een ziua a \u0219aptea s\u0103 te odihne\u0219ti, pentru ca boul \u0219i m\u0103garul t\u0103u s\u0103 se odihneasc\u0103, iar fiul sclavei tale \u0219i str\u0103inul s\u0103 se \u00eenvioreze.",
+    "S\u0103 fi\u021bi aten\u021bi la tot ce v-am spus. S\u0103 nu chema\u021bi numele altor dumnezei \u0219i s\u0103 nu se aud\u0103 din gura voastr\u0103.",
+    "De trei ori pe an s\u0103-Mi \u021bii o s\u0103rb\u0103toare.",
+    "S\u0103 p\u0103ze\u0219ti S\u0103rb\u0103toarea Azimelor. Timp de \u0219apte zile s\u0103 m\u0103n\u00e2nci azime, a\u0219a cum \u021bi-am poruncit, la timpul hot\u0103r\u00e2t \u00een luna Abib, c\u0103ci atunci ai ie\u0219it din Egipt. Nimeni s\u0103 nu se \u00eenf\u0103\u021bi\u0219eze \u00eenaintea Mea cu m\u00e2na goal\u0103.",
+    "S\u0103 p\u0103ze\u0219ti S\u0103rb\u0103toarea Seceri\u0219ului, cu primele roade ale muncii tale, din ceea ce ai sem\u0103nat pe c\u00e2mp, \u0219i S\u0103rb\u0103toarea Str\u00e2ngerii Roadelor, la sf\u00e2r\u0219itul anului, c\u00e2nd \u00ee\u021bi str\u00e2ngi rodul muncii de pe c\u00e2mp.",
+    "De trei ori pe an, to\u021bi b\u0103rba\u021bii t\u0103i s\u0103 se \u00eenf\u0103\u021bi\u0219eze \u00eenaintea St\u0103p\u00e2nului, DOMNUL.",
+    "S\u0103 nu aduci s\u00e2ngele jertfei Mele \u00eempreun\u0103 cu p\u00e2ine dospit\u0103, iar gr\u0103simea s\u0103rb\u0103torii Mele s\u0103 nu r\u0103m\u00e2n\u0103 peste noapte p\u00e2n\u0103 diminea\u021ba.",
+    "S\u0103 aduci cele dint\u00e2i dintre primele roade ale p\u0103m\u00e2ntului t\u0103u \u00een Casa DOMNULUI, Dumnezeul t\u0103u. S\u0103 nu fierbi un ied \u00een laptele mamei sale.",
+    "Iat\u0103, trimit un \u00cenger \u00eenaintea ta, ca s\u0103 te p\u0103zeasc\u0103 pe drum \u0219i s\u0103 te duc\u0103 \u00een locul pe care l-am preg\u0103tit.",
+    "Fii atent \u00eenaintea lui \u0219i ascult\u0103-i glasul. S\u0103 nu te r\u0103zvr\u0103te\u0219ti \u00eempotriva lui, c\u0103ci nu v\u0103 va ierta r\u0103zvr\u0103tirea, fiindc\u0103 Numele Meu este \u00een el.",
+    "Dar dac\u0103 \u00eei vei asculta cu adev\u0103rat glasul \u0219i vei face tot ce spun Eu, voi fi du\u0219manul du\u0219manilor t\u0103i \u0219i potrivnicul potrivnicilor t\u0103i.",
+    "\u00cengerul Meu va merge \u00eenaintea ta \u0219i te va duce la amori\u021bi, heti\u021bi, perizi\u021bi, canaani\u021bi, hevi\u021bi \u0219i iebusi\u021bi, iar Eu \u00eei voi nimici.",
+    "S\u0103 nu te pleci \u00eenaintea dumnezeilor lor, s\u0103 nu le sluje\u0219ti \u0219i s\u0103 nu urmezi practicile lor, ci s\u0103-i d\u0103r\u00e2mi cu des\u0103v\u00e2r\u0219ire \u0219i s\u0103 le sf\u0103r\u00e2mi st\u00e2lpii sacri.",
+    "S\u0103-I sluji\u021bi DOMNULUI, Dumnezeul vostru, iar El \u00ee\u021bi va binecuv\u00e2nta p\u00e2inea \u0219i apa. Voi \u00eendep\u0103rta boala din mijlocul t\u0103u.",
+    "\u00cen \u021bara ta nu va fi femeie care s\u0103 piard\u0103 sarcina sau s\u0103 fie stearp\u0103. Voi \u00eemplini num\u0103rul zilelor tale.",
+    "Voi trimite groaza Mea \u00eenaintea ta \u0219i voi arunca \u00een confuzie orice popor la care vei ajunge; \u00eei voi face pe to\u021bi du\u0219manii t\u0103i s\u0103-\u021bi \u00eentoarc\u0103 spatele.",
+    "Voi trimite viespea \u00eenaintea ta, iar ea \u00eei va izgoni dinaintea ta pe hevi\u021bi, canaani\u021bi \u0219i heti\u021bi.",
+    "Nu-i voi izgoni dinaintea ta \u00eentr-un singur an, pentru ca \u021bara s\u0103 nu ajung\u0103 pustie \u0219i animalele s\u0103lbatice s\u0103 nu se \u00eenmul\u021beasc\u0103 \u00eempotriva ta.",
+    "\u00cei voi izgoni pu\u021bin c\u00e2te pu\u021bin dinaintea ta, p\u00e2n\u0103 c\u00e2nd te vei \u00eenmul\u021bi \u0219i vei lua \u021bara \u00een st\u0103p\u00e2nire.",
+    "\u00ce\u021bi voi stabili hotarul de la Marea Ro\u0219ie p\u00e2n\u0103 la marea filistenilor \u0219i de la pustie p\u00e2n\u0103 la R\u00e2u. \u00cei voi da pe locuitorii \u021b\u0103rii \u00een m\u00e2na voastr\u0103, iar tu \u00eei vei izgoni dinaintea ta.",
+    "S\u0103 nu \u00eenchei leg\u0103m\u00e2nt cu ei \u0219i nici cu dumnezeii lor.",
+    "Ei s\u0103 nu locuiasc\u0103 \u00een \u021bara ta, ca s\u0103 nu te fac\u0103 s\u0103 p\u0103c\u0103tuie\u0219ti \u00eempotriva Mea, c\u0103ci slujirea dumnezeilor lor va deveni pentru tine o curs\u0103.\u201d",
+  ],
 }
