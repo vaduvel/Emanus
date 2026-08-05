883d381aebb4e0fbdbcc15ccfedaff943f37c4cf
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 19:23:38 +0300

    Adauga numeri.ts - cartea Numeri agregata, toate cele 36 de capitole

---
+++ packages/shared/src/bible/numeri.ts
@@ -0,0 +1,132 @@
+import type { BibleBook } from "./types.js"
+import { NUMERI_1 } from "./numeri1.js"
+import { NUMERI_2 } from "./numeri2.js"
+import { NUMERI_3 } from "./numeri3.js"
+import { NUMERI_4 } from "./numeri4.js"
+import { NUMERI_5 } from "./numeri5.js"
+import { NUMERI_6 } from "./numeri6.js"
+import { NUMERI_7 } from "./numeri7.js"
+import { NUMERI_8 } from "./numeri8.js"
+import { NUMERI_9 } from "./numeri9.js"
+import { NUMERI_10 } from "./numeri10.js"
+import { NUMERI_11 } from "./numeri11.js"
+import { NUMERI_12 } from "./numeri12.js"
+import { NUMERI_13 } from "./numeri13.js"
+import { NUMERI_14 } from "./numeri14.js"
+import { NUMERI_15 } from "./numeri15.js"
+import { NUMERI_16 } from "./numeri16.js"
+import { NUMERI_17 } from "./numeri17.js"
+import { NUMERI_18 } from "./numeri18.js"
+import { NUMERI_19 } from "./numeri19.js"
+import { NUMERI_20 } from "./numeri20.js"
+import { NUMERI_21 } from "./numeri21.js"
+import { NUMERI_22 } from "./numeri22.js"
+import { NUMERI_23 } from "./numeri23.js"
+import { NUMERI_24 } from "./numeri24.js"
+import { NUMERI_25 } from "./numeri25.js"
+import { NUMERI_26 } from "./numeri26.js"
+import { NUMERI_27 } from "./numeri27.js"
+import { NUMERI_28 } from "./numeri28.js"
+import { NUMERI_29 } from "./numeri29.js"
+import { NUMERI_30 } from "./numeri30.js"
+import { NUMERI_31 } from "./numeri31.js"
+import { NUMERI_32 } from "./numeri32.js"
+import { NUMERI_33 } from "./numeri33.js"
+import { NUMERI_34 } from "./numeri34.js"
+import { NUMERI_35 } from "./numeri35.js"
+import { NUMERI_36 } from "./numeri36.js"
+
+/*
+ * Cartea Numeri.
+ *
+ * Textul biblic stă în numeriText.ts (fișierele numeriTextN.ts).
+ * Explicația stă în fișierele de capitol: numeri1.ts, numeri2.ts, numeri3.ts, ...
+ *
+ * Fișierul acesta doar adună cartea. Toate cele treizeci și șase de capitole
+ * sunt importate aici și așezate în lista `chapters`, ca să nu fie nevoie de
+ * atins index.ts pentru fiecare capitol în parte.
+ */
+
+export { NUMERI_1 } from "./numeri1.js"
+export { NUMERI_2 } from "./numeri2.js"
+export { NUMERI_3 } from "./numeri3.js"
+export { NUMERI_4 } from "./numeri4.js"
+export { NUMERI_5 } from "./numeri5.js"
+export { NUMERI_6 } from "./numeri6.js"
+export { NUMERI_7 } from "./numeri7.js"
+export { NUMERI_8 } from "./numeri8.js"
+export { NUMERI_9 } from "./numeri9.js"
+export { NUMERI_10 } from "./numeri10.js"
+export { NUMERI_11 } from "./numeri11.js"
+export { NUMERI_12 } from "./numeri12.js"
+export { NUMERI_13 } from "./numeri13.js"
+export { NUMERI_14 } from "./numeri14.js"
+export { NUMERI_15 } from "./numeri15.js"
+export { NUMERI_16 } from "./numeri16.js"
+export { NUMERI_17 } from "./numeri17.js"
+export { NUMERI_18 } from "./numeri18.js"
+export { NUMERI_19 } from "./numeri19.js"
+export { NUMERI_20 } from "./numeri20.js"
+export { NUMERI_21 } from "./numeri21.js"
+export { NUMERI_22 } from "./numeri22.js"
+export { NUMERI_23 } from "./numeri23.js"
+export { NUMERI_24 } from "./numeri24.js"
+export { NUMERI_25 } from "./numeri25.js"
+export { NUMERI_26 } from "./numeri26.js"
+export { NUMERI_27 } from "./numeri27.js"
+export { NUMERI_28 } from "./numeri28.js"
+export { NUMERI_29 } from "./numeri29.js"
+export { NUMERI_30 } from "./numeri30.js"
+export { NUMERI_31 } from "./numeri31.js"
+export { NUMERI_32 } from "./numeri32.js"
+export { NUMERI_33 } from "./numeri33.js"
+export { NUMERI_34 } from "./numeri34.js"
+export { NUMERI_35 } from "./numeri35.js"
+export { NUMERI_36 } from "./numeri36.js"
+
+export const NUMERI: BibleBook = {
+  id: "numeri",
+  name: "Numeri",
+  testament: "vt",
+  order: 4,
+  blurb:
+    "Cartea numărătorilor și a drumului prin pustie. Israel este numărat de două ori, cade de multe ori, dar DOMNUL rămâne credincios: hrănește, judecă, sfințește, corectează și, în cele din urmă, aduce poporul la marginea țării făgăduite, pregătit pentru trecerea Iordanului.",
+  chapters: [
+    NUMERI_1,
+    NUMERI_2,
+    NUMERI_3,
+    NUMERI_4,
+    NUMERI_5,
+    NUMERI_6,
+    NUMERI_7,
+    NUMERI_8,
+    NUMERI_9,
+    NUMERI_10,
+    NUMERI_11,
+    NUMERI_12,
+    NUMERI_13,
+    NUMERI_14,
+    NUMERI_15,
+    NUMERI_16,
+    NUMERI_17,
+    NUMERI_18,
+    NUMERI_19,
+    NUMERI_20,
+    NUMERI_21,
+    NUMERI_22,
+    NUMERI_23,
+    NUMERI_24,
+    NUMERI_25,
+    NUMERI_26,
+    NUMERI_27,
+    NUMERI_28,
+    NUMERI_29,
+    NUMERI_30,
+    NUMERI_31,
+    NUMERI_32,
+    NUMERI_33,
+    NUMERI_34,
+    NUMERI_35,
+    NUMERI_36,
+  ],
+}
