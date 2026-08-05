2b94a06bfe763bf6ec36ecbf38e43009a42393f7
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:38:53 +0300

    Numeri 15: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText15.ts
@@ -0,0 +1,53 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 15.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.15.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 41 de versete.
+ */
+
+export const NUMERI_TEXT_15: Record<number, readonly string[]> = {
+  15: [
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eSpune-le fiilor lui Israel \u0219i vorbe\u015fte-le: C\u00e2nd ve\u021bi intra \u00een \u021bara locuin\u021belor voastre, pe care Eu vi o dau,",
+    "\u0219i ve\u021bi aduce DOMNULUI o jertf\u0103 mistuit\u0103 de foc, o ardere-de-tot sau o jertf\u0103 pentru \u00eemplinirea unui jur\u0103m\u00e2nt sau ca ofrand\u0103 de bun\u0103voie, sau la s\u0103rb\u0103torile voastre r\u00e2nduite, ca s\u0103 face\u021bi un miros pl\u0103cut DOMNULUI din cirezi sau din turme,",
+    "cel ce \u00ee\u0219i va aduce ofranda DOMNULUI s\u0103 aduc\u0103 ca dar de cereale a zecea parte dintr-o ef\u0103 de f\u0103in\u0103 aleas\u0103, fr\u0103m\u00e2ntat\u0103 cu un sfert de hin de ulei;",
+    "iar ca dar de b\u0103utur\u0103 s\u0103 aduci un sfert de hin de vin pe l\u00e2ng\u0103 arderea-de-tot sau pe l\u00e2ng\u0103 jertf\u0103, pentru fiecare miel.",
+    "Pentru un berbec, s\u0103 preg\u0103te\u015fti ca dar de cereale dou\u0103 zecimi dintr-o ef\u0103 de f\u0103in\u0103 aleas\u0103, fr\u0103m\u00e2ntat\u0103 cu o treime de hin de ulei;",
+    "iar ca dar de b\u0103utur\u0103 s\u0103 aduci o treime de hin de vin, ca miros pl\u0103cut DOMNULUI.",
+    "C\u00e2nd preg\u0103te\u015fti un taur ca ardere-de-tot sau ca jertf\u0103 pentru \u00eemplinirea unui jur\u0103m\u00e2nt, sau ca jertf\u0103 de pace DOMNULUI,",
+    "s\u0103 aduci pe l\u00e2ng\u0103 taur un dar de cereale de trei zecimi de ef\u0103 de f\u0103in\u0103 aleas\u0103, fr\u0103m\u00e2ntat\u0103 cu jum\u0103tate de hin de ulei;",
+    "iar ca dar de b\u0103utur\u0103 s\u0103 aduci jum\u0103tate de hin de vin; aceasta este o jertf\u0103 prin foc, de un miros pl\u0103cut DOMNULUI.",
+    "A\u015fa s\u0103 se fac\u0103 pentru fiecare taur, pentru fiecare berbec, pentru fiecare miel sau ied.",
+    "Dup\u0103 num\u0103rul animalelor pe care le ve\u021bi preg\u0103ti, a\u015fa s\u0103 face\u021bi pentru fiecare, dup\u0103 num\u0103rul lor.",
+    "Orice b\u0103\u015ftin\u0103\u015f s\u0103 fac\u0103 aceste lucruri a\u015fa, c\u00e2nd aduce o jertf\u0103 prin foc de un miros pl\u0103cut DOMNULUI.",
+    "Dac\u0103 un str\u0103in locuie\u015fte la voi sau cineva se afl\u0103 \u00een mijlocul vostru \u00een neamurile voastre \u0219i aduce o jertf\u0103 prin foc de un miros pl\u0103cut DOMNULUI, cum face\u021bi voi, a\u015fa s\u0103 fac\u0103 \u0219i el.",
+    "S\u0103 fie o singur\u0103 or\u00e2nduial\u0103 pentru adunare \u0219i pentru str\u0103inul care locuie\u015fte la voi, o or\u00e2nduial\u0103 ve\u015fnic\u0103 \u00een neamurile voastre: cum sunte\u021bi voi, a\u015fa va fi \u0219i str\u0103inul \u00eenaintea DOMNULUI.",
+    "O singur\u0103 lege \u0219i o singur\u0103 r\u00e2nduial\u0103 s\u0103 fie pentru voi \u0219i pentru str\u0103inul care locuie\u015fte la voi.\u201d",
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eSpune-le fiilor lui Israel \u0219i vorbe\u015fte-le: C\u00e2nd ve\u021bi ajunge \u00een \u021bara \u00een care v\u0103 duc,",
+    "c\u00e2nd ve\u021bi m\u00e2nca din p\u00e2inea \u021b\u0103rii, s\u0103 aduce\u021bi un prinos DOMNULUI.",
+    "Din prima voastr\u0103 maia s\u0103 aduce\u021bi o turt\u0103 ca prinos; cum aduce\u021bi prinosul din arie, a\u015fa s\u0103 o aduce\u021bi.",
+    "Din prima voastr\u0103 maia s\u0103-I da\u021bi DOMNULUI un prinos \u00een neamurile voastre.",
+    "Dac\u0103 ve\u021bi gre\u015fi din ne\u015ftiin\u021b\u0103 \u0219i nu ve\u021bi \u00eemplini toate aceste porunci pe care le-a vorbit DOMNUL lui Moise,",
+    "tot ce v-a poruncit DOMNUL prin Moise, din ziua \u00een care DOMNUL a dat porunc\u0103 \u0219i \u00een neamurile voastre,",
+    "dac\u0103 fapta s-a f\u0103cut din ne\u015ftiin\u021b\u0103, f\u0103r\u0103 \u015ftirea adun\u0103rii, toat\u0103 adunarea s\u0103 aduc\u0103 un taur t\u00e2n\u0103r ca ardere-de-tot de un miros pl\u0103cut DOMNULUI, cu jertfa lui de m\u00e2ncare \u0219i jertfa lui de turnare, dup\u0103 r\u00e2nduial\u0103, \u0219i un \u021bap ca jertf\u0103 pentru p\u0103cat.",
+    "Preotul s\u0103 fac\u0103 isp\u0103\u015fire pentru toat\u0103 adunarea fiilor lui Israel \u0219i li se va ierta; c\u0103ci a fost o gre\u015feal\u0103 din ne\u015ftiin\u021b\u0103 \u0219i ei \u0219i-au adus ofranda, o jertf\u0103 prin foc DOMNULUI, \u0219i jertfa lor pentru p\u0103cat \u00eenaintea DOMNULUI pentru gre\u015feala lor.",
+    "I se va ierta \u00eentregii adun\u0103ri a fiilor lui Israel \u0219i str\u0103inului care locuie\u015fte printre ei, c\u0103ci tot poporul a gre\u015fit din ne\u015ftiin\u021b\u0103.",
+    "Dac\u0103 o singur\u0103 persoan\u0103 va p\u0103c\u0103tui din ne\u015ftiin\u021b\u0103, s\u0103 aduc\u0103 o capr\u0103 de un an ca jertf\u0103 pentru p\u0103cat.",
+    "Preotul s\u0103 fac\u0103 isp\u0103\u015fire \u00eenaintea DOMNULUI pentru persoana care a gre\u015fit din ne\u015ftiin\u021b\u0103; c\u00e2nd se va face isp\u0103\u015fire pentru ea, i se va ierta.",
+    "At\u00e2t pentru cel b\u0103\u015ftin\u0103\u015f printre fiii lui Israel, c\u00e2t \u0219i pentru str\u0103inul care locuie\u015fte printre ei, s\u0103 ave\u021bi o singur\u0103 lege pentru cel ce p\u0103c\u0103tuie\u015fte din ne\u015ftiin\u021b\u0103.",
+    "Dar persoana care face ceva cu voin\u021b\u0103 sfid\u0103toare, fie b\u0103\u015ftin\u0103\u015f, fie str\u0103in, \u00celi hulte\u015fte pe DOMNUL; persoana aceea va fi nimicit\u0103 din mijlocul poporului s\u0103u.",
+    "Pentru c\u0103 a disprtu it cuv\u00e2ntul DOMNULUI \u0219i a c\u0103lcat porunca Lui, persoana aceea va fi nimicit\u0103 cu totul; nelegiuirea ei r\u0103m\u00e2ne asupra ei.\u201d",
+    "Pe c\u00e2nd fiii lui Israel erau \u00een pustie, au g\u0103sit un om str\u00e2ng\u00e2nd lemne \u00een ziua Sabatului.",
+    "Cei ce l-au g\u0103sit str\u00e2ng\u00e2nd lemne l-au adus la Moise, la Aaron \u0219i la toat\u0103 adunarea.",
+    "L-au pus sub paz\u0103, pentru c\u0103 nu se hot\u0103r\u00e2se \u00eenc\u0103 ce trebuia s\u0103 i se fac\u0103.",
+    "DOMNUL i-a spus lui Moise: \u201eOmul acesta s\u0103 fie dat la moarte; toat\u0103 adunarea s\u0103-l ucid\u0103 cu pietre afar\u0103 din tab\u0103r\u0103.\u201d",
+    "Toat\u0103 adunarea l-a scos afar\u0103 din tab\u0103r\u0103 \u0219i l-au ucis cu pietre, \u0219i a murit, a\u015fa cum \u00eei poruncise DOMNUL lui Moise.",
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eSpune-le fiilor lui Israel \u0219i vorbe\u015fte-le s\u0103-\u0219i fac\u0103 ciucuri la col\u021burile hainelor lor, \u00een toate neamurile lor, \u0219i s\u0103 pun\u0103 pe ciucurele de la fiecare col\u021b un fir albastru.",
+    "S\u0103 ave\u021bi ciucurele acesta ca, privindu-l, s\u0103 v\u0103 aduce\u021bi aminte de toate poruncile DOMNULUI \u0219i s\u0103 le \u00eemplini\u021bi, \u0219i s\u0103 nu urma\u021bi poftelor inimilor voastre \u0219i ochilor vo\u015ftri care v\u0103 duc la infidelitate;",
+    "ca s\u0103 v\u0103 aduce\u021bi aminte \u0219i s\u0103 \u00eemplini\u021bi toate poruncile Mele \u0219i s\u0103 fi\u021bi sfin\u021bi pentru Dumnezeul vostru.",
+    "Eu sunt DOMNUL, Dumnezeul vostru, care v-am scos din \u021bara Egiptului ca s\u0103 fiu Dumnezeul vostru: Eu sunt DOMNUL, Dumnezeul vostru.\u201d",
+  ],
+}
