d50bc80fff8efb6bb8830d1237bad70617dd3928
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:58:40 +0300

    Numeri 30: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText30.ts
@@ -0,0 +1,28 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 30.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.30.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 16 versete.
+ */
+
+export const NUMERI_TEXT_30: Record<number, readonly string[]> = {
+  30: [
+    "Moise le-a vorbit c\u0103peteniilor semin\u021biilor fiilor lui Israel, spun\u00e2nd: \u201eIat\u0103 ce a poruncit DOMNUL:",
+    "C\u00e2nd un b\u0103rbat va face un jur\u0103m\u00e2nt DOMNULUI sau va depune un jur\u0103m\u00e2nt prin care se leag\u0103 cu o f\u0103g\u0103duin\u021b\u0103, s\u0103 nu-\u015fi calce cuv\u00e2ntul, ci s\u0103 fac\u0103 dup\u0103 tot ce i-a ie\u015fit din gur\u0103.",
+    "C\u00e2nd o femeie va face un jur\u0103m\u00e2nt DOMNULUI \u0219i se va lega cu o f\u0103g\u0103duin\u021b\u0103 \u00een tinere\u021bea ei, \u00een casa tat\u0103lui ei,",
+    "\u0219i tat\u0103l ei va auzi de jur\u0103m\u00e2ntul ei \u0219i de f\u0103g\u0103duin\u021ba cu care s-a legat, \u0219i tat\u0103l ei va t\u0103cea \u00een privinn\u021ba ei, atunci toate jur\u0103mintele ei vor r\u0103m\u00e2ne valabile \u0219i orice f\u0103g\u0103duin\u021b\u0103 cu care s-a legat va r\u0103m\u00e2ne valabil\u0103.",
+    "Dar dac\u0103 tat\u0103l ei se va \u00eempotriva \u00een ziua \u00een care va auzi de ea, niciunul din jur\u0103mintele ei sau din f\u0103g\u0103duin\u021bele cu care s-a legat nu va r\u0103m\u00e2ne valabil; \u0219i DOMNUL o va ierta, pentru c\u0103 tat\u0103l ei s-a \u00eempotriva.",
+    "Dac\u0103 se va c\u0103s\u0103tori fiind legat\u0103 prin jur\u0103mintele ei sau prin vreo vorb\u0103 ie\u015fit\u0103 nechibzuit din buzele ei cu care s-a legat,",
+    "\u0219i so\u021bul ei va auzi de lucrul acesta \u0219i va t\u0103cea \u00een privinna ei \u00een ziua \u00een care va auzi, atunci jur\u0103mintele ei vor r\u0103m\u00e2ne valabile \u0219i f\u0103g\u0103duin\u021bele cu care s-a legat vor r\u0103m\u00e2ne valabile.",
+    "Dar dac\u0103 so\u021bul ei se va \u00eempotriva \u00een ziua \u00een care va auzi de lucrul acesta, el va desfiinna jur\u0103m\u00e2ntul pe care l-a f\u0103cut ea \u0219i vorba ie\u015fit\u0103 nechibzuit din buzele ei cu care s-a legat; \u0219i DOMNUL o va ierta.",
+    "Orice jur\u0103m\u00e2nt al unei v\u0103duve sau al unei femei divor\u021bate, tot ce \u015fi-a impus ea \u00een mod ferm va r\u0103m\u00e2ne valabil pentru ea.",
+    "Dac\u0103 o femeie a f\u0103cut un jur\u0103m\u00e2nt \u00een casa so\u021bului ei sau s-a legat cu o f\u0103g\u0103duin\u021b\u0103 prin jur\u0103m\u00e2nt,",
+    "\u0219i so\u021bul ei a auzit \u0219i a t\u0103cut \u00een privinna ei, \u0219i nu s-a \u00eempotrivit, atunci toate jur\u0103mintele ei vor r\u0103m\u00e2ne valabile \u0219i orice f\u0103g\u0103duin\u021b\u0103 cu care s-a legat va r\u0103m\u00e2ne valabil\u0103.",
+    "Dar dac\u0103 so\u021bul ei le-a desfiinn\u021bat \u00een mod l\u0103murit \u00een ziua \u00een care le-a auzit, tot ce a ie\u015fit din buzele ei, fie jur\u0103minte, fie f\u0103g\u0103duin\u021b\u0103 legat\u0103, nu va r\u0103m\u00e2ne valabil; so\u021bul ei le-a desfiinn\u021bat \u0219i DOMNUL o va ierta.",
+    "So\u021bul ei poate s\u0103 \u00eent\u0103reasc\u0103 sau so\u021bul ei poate s\u0103 desfiinn\u021beze orice jur\u0103m\u00e2nt \u0219i orice leg\u0103m\u00e2nt prin jur\u0103m\u00e2nt de smerire a sufletului.",
+    "Dac\u0103 \u00een s\u0103 so\u021bul ei tace din zi \u00een zi \u00een privinna ei, el \u00eent\u0103re\u015fte astfel toate jur\u0103mintele ei sau toate f\u0103g\u0103duin\u021bele care sunt asupra ei; le \u00eent\u0103re\u015fte pentru c\u0103 a t\u0103cut \u00een privinna ei \u00een ziua \u00een care a auzit.",
+    "Dar dac\u0103 le va desfiinna dup\u0103 un timp de la ziua \u00een care le-a auzit, el \u00ee\u015fi va purta nelegiuirea.\u201d",
+    "Acestea sunt or\u00e2nduirile pe care le-a poruncit DOMNUL lui Moise \u00eentre un b\u0103rbat \u0219i so\u021bia lui, \u00eentre un tat\u0103 \u0219i fiica lui \u00een tinere\u021bea ei, \u00een casa tat\u0103lui ei.",
+  ],
+}
