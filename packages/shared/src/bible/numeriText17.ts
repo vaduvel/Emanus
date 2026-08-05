3da3549af922284a155f0b65175d7d1befbc92f2
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:41:42 +0300

    Numeri 17: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText17.ts
@@ -0,0 +1,25 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 17.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.17.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 13 versete.
+ */
+
+export const NUMERI_TEXT_17: Record<number, readonly string[]> = {
+  17: [
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eVorbe\u015fte fiilor lui Israel \u0219i ia de la ei c\u00e2te un toiag de fiecare cas\u0103 p\u0103rinteasc\u0103, doisprezece toiege de la toate c\u0103peteniile lor, dup\u0103 casele p\u0103rin\u021bilor lor; s\u0103 scrii numele fiec\u0103ruia pe toiagul lui.",
+    "\u015ei s\u0103 scrii numele lui Aaron pe toiagul lui Levi; c\u0103ci va fi c\u00e2te un toiag pentru fiecare cap al caselor p\u0103rin\u021bilor lor.",
+    "S\u0103 le pui \u00een Cortul \u00cent\u00e2lnirii, \u00eenaintea m\u0103rturiei, unde M\u0103 \u00eent\u00e2lnesc cu voi.",
+    "Toiagul omului pe care-l voi alege va odr\u0103sli; \u0219i astfel voi face s\u0103 \u00eenceteze dinaintea Mea c\u00e2rtirile fiilor lui Israel, pe care le rostesc \u00eempotriva voastr\u0103.\u201d",
+    "Moise le-a vorbit fiilor lui Israel; \u0219i toate c\u0103peteniile lor i-au dat c\u00e2te un toiag, fiecare c\u0103petenie c\u00e2te un toiag, dup\u0103 casele p\u0103rin\u021bilor lor, adic\u0103 doisprezece toiege; iar toiagul lui Aaron era \u00een mijlocul toiegelor lor.",
+    "Moise a pus toiegele \u00eenaintea DOMNULUI \u00een Cortul M\u0103rturiei.",
+    "A doua zi, c\u00e2nd a intrat Moise \u00een Cortul M\u0103rturiei, iat\u0103 c\u0103 toiagul lui Aaron pentru casa lui Levi odr\u0103slise: \u00eenverzise, \u00eenflor\u00eese \u0219i f\u0103cuse migdale.",
+    "Moise a scos toate toiegele dinaintea DOMNULUI \u0219i le-a dus tuturor fiilor lui Israel; ei le-au v\u0103zut \u0219i \u0219i-au luat fiecare toiagul.",
+    "DOMNUL i-a spus lui Moise: \u201eAdu toiagul lui Aaron \u00eeinapoi \u00eenaintea m\u0103rturiei, ca s\u0103 fie p\u0103strat ca un semn pentru fiii r\u0103zvr\u0103ti\u021bi, ca s\u0103 pui cap\u0103t c\u00e2rtirilor lor \u00eempotriva Mea \u0219i s\u0103 nu moar\u0103.\u201d",
+    "Moise a f\u0103cut a\u015fa; cum \u00eei poruncise DOMNUL, a\u015fa a f\u0103cut.",
+    "Fiii lui Israel i-au spus lui Moise: \u201eIat\u0103, pierim, suntem pierdu\u021bi, to\u021bi suntem pierdu\u021bi!",
+    "Oricine se apropie, cine se apropie de Cortul DOMNULUI moare! Vom pieri noi oare to\u021bi?\u201d",
+  ],
+}
