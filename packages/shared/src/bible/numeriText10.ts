1d437685726652f47262eaaf3a1f0e1e825bfbf5
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:31:32 +0300

    Numeri 10: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText10.ts
@@ -0,0 +1,48 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 10.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.10.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 36 de versete.
+ */
+
+export const NUMERI_TEXT_10: Record<number, readonly string[]> = {
+  10: [
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eFa-\u021bi dou\u0103 tr\u00e2mbi\u021be de argint; s\u0103 le faci din argint b\u0103tut. Ele \u00ee\u021bi vor sluji la chemarea adun\u0103rii \u0219i la pornirea taberelor.",
+    "C\u00e2nd se va suna din am\u00e2ndou\u0103, toat\u0103 adunarea s\u0103 se str\u00e2ng\u0103 la tine la intrarea Cortului \u00cent\u00e2lnirii.",
+    "Dac\u0103 se va suna doar din una singur\u0103, c\u0103peteniile, capetele miilor lui Israel, s\u0103 se str\u00e2ng\u0103 la tine.",
+    "C\u00e2nd ve\u021bi suna cu semnale de alarm\u0103, s\u0103 porneasc\u0103 taberele care t\u0103b\u0103r\u0103sc la r\u0103s\u0103rit.",
+    "C\u00e2nd ve\u021bi suna a doua oar\u0103 cu semnal de alarm\u0103, s\u0103 porneasc\u0103 taberele care t\u0103b\u0103r\u0103sc la sud; s\u0103 se sune cu alarm\u0103 pentru plecarea lor.",
+    "Dar c\u00e2nd este vorba de str\u00e2ngerea adun\u0103rii, s\u0103 suna\u021bi f\u0103r\u0103 s\u0103 da\u021bi semnal de alarm\u0103.",
+    "Fiii lui Aaron, preo\u021bii, s\u0103 sune din tr\u00e2mbi\u021be; aceasta va fi o or\u00e2nduial\u0103 ve\u015fnic\u0103 pentru voi \u00een neamurile voastre.",
+    "C\u00e2nd ve\u021bi merge la r\u0103zboi \u00een \u021bara voastr\u0103 \u00eempotriva vr\u0103jma\u015fului care v\u0103 \u00eempresoar\u0103, s\u0103 suna\u021bi din tr\u00e2mbi\u021be cu alarm\u0103; \u0219i v\u0103 ve\u021bi aduce aminte \u00eenaintea DOMNULUI, Dumnezeul vostru, \u0219i ve\u021bi fi izb\u0103vi\u021bi de vr\u0103jma\u015fii vo\u015ftri.",
+    "\u00cen ziua bucuriei voastre, la s\u0103rb\u0103torile voastre r\u00e2nduite \u0219i la lunile voastre noi, s\u0103 suna\u021bi din tr\u00e2mbi\u021be peste arderile-voastre-de-tot \u0219i peste jertfele voastre de pace; ele vor fi o aducere aminte de voi \u00eenaintea Dumnezeului vostru: Eu sunt DOMNUL, Dumnezeul vostru.\u201d",
+    "\u00cen al doilea an, \u00een luna a doua, \u00een ziua a dou\u0103zecea a lunii, norul s-a ridicat de pe Cortul M\u0103rturiei.",
+    "\u015ei fiii lui Israel au pornit \u00een c\u0103l\u0103toriile lor din pustia Sinai; iar norul s-a oprit \u00een pustia Paran.",
+    "Astfel au pornit pentru prima dat\u0103, dup\u0103 porunca DOMNULUI dat\u0103 prin Moise.",
+    "A pornit mai \u00eent\u00e2i steagul taberei fiilor lui Iuda, dup\u0103 o\u0219tirile lor; peste o\u0219tirea lui era Nah\u015fon, fiul lui Amminadab.",
+    "Peste o\u0219tirea semin\u021biei fiilor lui Isahar era Netanel, fiul lui \u0162uar.",
+    "Peste o\u0219tirea semin\u021biei fiilor lui Zabulon era Eliab, fiul lui Helon.",
+    "Cortul a fost cobor\u00e2t; \u0219i fiii lui Gher\u015fon \u0219i fiii lui Merari au pornit purt\u00e2nd Cortul.",
+    "A pornit steagul taberei lui Ruben, dup\u0103 o\u0219tirile lor; peste o\u0219tirea lui era Elizur, fiul lui \u015eedeur.",
+    "Peste o\u0219tirea semin\u021biei fiilor lui Simeon era \u015eelumiel, fiul lui \u0162uri\u015fadai.",
+    "Peste o\u0219tirea semin\u021biei fiilor lui Gad era Eliasaf, fiul lui Deuel.",
+    "Au pornit chehat i\u021bii purt\u00e2nd Sf\u00e2ntul L\u0103ca\u015f; iar celelalte lucruri ale Cortului se ridicau p\u00e2n\u0103 la sosirea lor.",
+    "A pornit steagul taberei fiilor lui Efraim, dup\u0103 o\u0219tirile lor; peste o\u0219tirea lui era Eli\u015fama, fiul lui Ammihud.",
+    "Peste o\u0219tirea semin\u021biei fiilor lui Manase era Gamaliel, fiul lui Peda\u021bur.",
+    "Peste o\u0219tirea semin\u021biei fiilor lui Beniamin era Abidan, fiul lui Gideoni.",
+    "A pornit steagul taberei fiilor lui Dan, str\u00e2ng\u00e2nd toate taberele, dup\u0103 o\u0219tirile lor; peste o\u0219tirea lui era Ahiezer, fiul lui Ammi\u015fadai.",
+    "Peste o\u0219tirea semin\u021biei fiilor lui A\u015fer era Paghiel, fiul lui Ocran.",
+    "Peste o\u0219tirea semin\u021biei fiilor lui Neftali era Ahira, fiul lui Enan.",
+    "Aceasta a fost ordinea de pornire a fiilor lui Israel, dup\u0103 o\u0219tirile lor, c\u00e2nd au plecat.",
+    "Moise i-a spus lui Hobab, fiul lui Reuel madianitul, socrul lui Moise: \u201eNoi pornim spre locul despre care DOMNUL a zis: \u00abVi-l voi da.\u00bb Vino cu noi \u0219i-\u021bi vom face bine, c\u0103ci DOMNUL a f\u0103g\u0103duit binele pentru Israel.\u201d",
+    "Dar el i-a r\u0103spuns: \u201eNu voi merge, ci m\u0103 voi duce \u00een \u021bara mea \u0219i la rudele mele.\u201d",
+    "Moise a zis: \u201eTe rog, nu ne p\u0103r\u0103si, c\u0103ci tu \u015ftii cum trebuie s\u0103 t\u0103b\u0103r\u00e2m \u00een pustie \u0219i ne vei fi ca un ochi.",
+    "\u015ei dac\u0103 vei veni cu noi, binele pe care DOMNUL ni-l va face, \u021bi-l vom face \u0219i \u021bie.\u201d",
+    "Au pornit de la muntele DOMNULUI cale de trei zile; iar chivotul leg\u0103m\u00e2ntului DOMNULUI a mers \u00eenaintea lor cale de trei zile, ca s\u0103 le caute un loc de odihn\u0103.",
+    "Norul DOMNULUI era deasupra lor ziua, c\u00e2nd porneau din tab\u0103r\u0103.",
+    "C\u00e2nd pornea chivotul, Moise zicea: \u201eRidic\u0103-Te, DOMNULE, \u0219i s\u0103 se \u00eempr\u0103\u015ftie vr\u0103jma\u015fii T\u0103i, \u0219i cei ce Te ur\u0103sc s\u0103 fug\u0103 dinaintea Fe\u021bei Tale!\u201d",
+    "Iar c\u00e2nd se oprea, zicea: \u201e\u00centoarce-Te, DOMNULE, la zecile de mii de mii ale lui Israel!\u201d",
+  ],
+}
