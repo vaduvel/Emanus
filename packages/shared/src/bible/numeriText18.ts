afccb3259d288c865efae323b3df07f67f63c27c
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:43:12 +0300

    Numeri 18: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText18.ts
@@ -0,0 +1,44 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 18.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.18.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 32 de versete.
+ */
+
+export const NUMERI_TEXT_18: Record<number, readonly string[]> = {
+  18: [
+    "DOMNUL i-a spus lui Aaron: \u201eTu, fiii t\u0103i \u0219i casa tat\u0103lui t\u0103u cu tine ve\u021bi purta nelegiuirea Sf\u00e2ntului L\u0103ca\u015f; tu \u0219i fiii t\u0103i cu tine ve\u021bi purta nelegiuirea preo\u021biei voastre.",
+    "Apropie de asemenea pe fra\u021bii t\u0103i, semin\u021bia lui Levi, semin\u021bia tat\u0103lui t\u0103u, ca s\u0103 se al\u0103ture \u021bie \u0219i s\u0103-\u021bi slujeasc\u0103; iar tu \u0219i fiii t\u0103i cu tine ve\u021bi sluji \u00eenaintea Cortului M\u0103rturiei.",
+    "Ei vor p\u0103zi paza ta \u0219i paza \u00eentregului Cort; dar de uneltele Sf\u00e2ntului L\u0103ca\u015f \u0219i de altar s\u0103 nu se apropie, ca s\u0103 nu moar\u0103, nici ei, nici voi.",
+    "Se vor al\u0103tura \u021bie \u0219i vor p\u0103zi paza Cortului \u00cent\u00e2lnirii pentru toat\u0103 slujba Cortului; iar str\u0103inul s\u0103 nu se apropie de voi.",
+    "Voi ve\u021bi p\u0103zi paza Sf\u00e2ntului L\u0103ca\u015f \u0219i paza altarului, ca s\u0103 nu mai fie m\u00e2nie peste fiii lui Israel.",
+    "Iat\u0103, Eu i-am luat pe fra\u021bii vo\u015ftri, levi\u021bii, din mijlocul fiilor lui Israel; ei v\u0103 sunt da\u021bi ca un dar pentru DOMNUL, ca s\u0103 fac\u0103 slujba Cortului \u00cent\u00e2lnirii.",
+    "Dar tu \u0219i fiii t\u0103i cu tine s\u0103 v\u0103 p\u0103zi\u021bi preo\u021bia \u00een tot ce prive\u015fte altarul \u0219i ce este dincolo de perdea, \u0219i s\u0103 sluji\u021bi acolo. V\u0103 dau preo\u021bia ca o slujb\u0103 de dar; iar str\u0103inul care se va apropia s\u0103 fie dat la moarte.\u201d",
+    "DOMNUL i-a vorbit lui Aaron: \u201eIat\u0103, Eu \u021bi-am dat \u00een grij\u0103 toate prinosurile Mele ridicate; din toate lucrurile sfinte ale fiilor lui Israel \u021bi le-am dat \u021bie \u0219i fiilor t\u0103i ca drept de ungere, ca o or\u00e2nduial\u0103 ve\u015fnic\u0103.",
+    "Iat\u0103 ce va fi al t\u0103u din lucrurile preasf\u00e2nte care nu sunt arse de foc: toate ofrandele lor, toate jertfele lor de m\u00e2ncare, toate jertfele lor pentru p\u0103cat \u0219i toate jertfele lor pentru vin\u0103 pe care Mi le vor aduce vor fi preasf\u00e2nte pentru tine \u0219i pentru fiii t\u0103i.",
+    "S\u0103 le m\u00e2nca\u021bi \u00eentr-un loc preasf\u00e2nt; orice b\u0103rbat s\u0103 m\u00e2nce din ele; ele vor fi sfinte pentru tine.",
+    "Iat\u0103 ce va mai fi al t\u0103u: prinosul ridicat din darurile lor, din toate jertfele leg\u0103nate ale fiilor lui Israel, \u021bi le-am dat \u021bie, fiilor t\u0103i \u0219i fiicelor tale cu tine, ca o or\u00e2nduial\u0103 ve\u015fnic\u0103. Oricine este curat \u00een casa ta va putea s\u0103 m\u00e2nce din ele.",
+    "Tot ce este mai bun din ulei, tot ce este mai bun din must \u0219i din gr\u00e2u, p\u00e2rga pe care o vor da DOMNULUI, \u021bi le-am dat \u021bie.",
+    "Cele dint\u00e2i roade din tot ce este pe p\u0103m\u00e2ntul lor, pe care le vor aduce DOMNULUI, vor fi ale tale. Oricine este curat \u00een casa ta va putea s\u0103 m\u00e2nce din ele.",
+    "Tot ce este d\u0103ruit prin jur\u0103m\u00e2nt \u00een Israel va fi al t\u0103u.",
+    "Orice \u00eent\u00e2i n\u0103scut din orice trup pe care \u00eel aduc DOMNULUI, fie om, fie animal, va fi al t\u0103u; \u00eens\u0103 pe \u00eent\u00e2iul n\u0103scut al omului \u00eel vei r\u0103scump\u0103ra neap\u0103rat, \u0219i pe \u00eent\u00e2iul n\u0103scut al animalului necurat \u00eel vei r\u0103scump\u0103ra.",
+    "R\u0103scump\u0103rarea o vei face de la v\u00e2rsta de o lun\u0103, dup\u0103 pre\u021buirea ta de cinci \u015fecheli de argint, dup\u0103 \u015fechelul Sf\u00e2ntului L\u0103ca\u015f, care este de dou\u0103zeci de ghera.",
+    "Dar pe \u00eent\u00e2iul n\u0103scut al vacii, pe \u00eent\u00e2iul n\u0103scut al oaiei sau pe \u00eent\u00e2iul n\u0103scut al caprei s\u0103 nu-l r\u0103scumperi: sunt sfinte. S\u00e2ngele lor s\u0103-l strope\u015fti pe altar \u0219i gr\u0103simea lor s\u0103 o arzi ca o jertf\u0103 prin foc de un miros pl\u0103cut DOMNULUI.",
+    "Iar carnea lor va fi a ta, ca \u0219i pieptul leg\u0103nat \u0219i ca spata dreapt\u0103.",
+    "Toate prinosurile ridicate din lucrurile sfinte pe care le aduc fiii lui Israel DOMNULUI \u021bi le-am dat \u021bie, fiilor t\u0103i \u0219i fiicelor tale cu tine, ca o or\u00e2nduial\u0103 ve\u015fnic\u0103. Acesta este un leg\u0103m\u00e2nt de sare ve\u015fnic \u00eenaintea DOMNULUI pentru tine \u0219i pentru urma\u015fii t\u0103i \u00eempreun\u0103 cu tine.\u201d",
+    "DOMNUL i-a spus lui Aaron: \u201eTu nu vei avea nicio mo\u015ftenire \u00een \u021bara lor \u0219i nu vei avea nicio parte \u00een mijlocul lor; Eu sunt partea ta \u0219i mo\u015ftenirea ta \u00een mijlocul fiilor lui Israel.",
+    "Iat\u0103 c\u0103 fiilor lui Levi le-am dat ca mo\u015ftenire toate zeciuielile din Israel, pentru slujba pe care o fac ei, slujba Cortului \u00cent\u00e2lnirii.",
+    "Fiii lui Israel s\u0103 nu se mai apropie de Cortul \u00cent\u00e2lnirii, ca s\u0103 nu se \u00eencarce de p\u0103cat \u0219i s\u0103 moar\u0103.",
+    "Ci levi\u021bii s\u0103 fac\u0103 slujba Cortului \u00cent\u00e2lnirii \u0219i ei vor purta nelegiuirea lor; aceasta va fi o or\u00e2nduial\u0103 ve\u015fnic\u0103 \u00een neamurile voastre. Ei nu vor avea nicio mo\u015ftenire \u00een mijlocul fiilor lui Israel;",
+    "c\u0103ci zeciuielile fiilor lui Israel pe care le aduc ca prinos ridicat DOMNULUI le-am dat ca mo\u015ftenire levi\u021bilor; de aceea le-am zis: \u00abNu vor avea nicio mo\u015ftenire \u00een mijlocul fiilor lui Israel.\u00bb\u201d",
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eVorbe\u015fte-le levi\u021bilor \u0219i spune-le: C\u00e2nd ve\u021bi lua de la fiii lui Israel zeciuiala pe care v-o dau din partea lor ca mo\u015ftenire a voastr\u0103, s\u0103 aduce\u021bi din ea un prinos ridicat DOMNULUI: a zecea parte din zeciuial\u0103.",
+    "Prinosul vostru ridicat v\u0103 va fi socotit ca gr\u00e2ul din arie \u0219i ca mustul din teasc.",
+    "Astfel s\u0103 aduce\u021bi \u0219i voi un prinos ridicat DOMNULUI din toate zeciuielile voastre pe care le lua\u021bi de la fiii lui Israel; \u0219i prinosul acesta ridicat pentru DOMNUL s\u0103-l da\u021bi preotului Aaron.",
+    "Din toate darurile care vi se dau s\u0103 aduce\u021bi toate prinosurile ridicate ale DOMNULUI, din tot ce este mai bun din ele, partea sfin\u021bit\u0103 a lor.",
+    "Spune-le: \u00abC\u00e2nd ve\u021bi aduce ce este mai bun din ea, levi\u021bilor li se va socoti ca venitul dintr-o arie \u0219i ca venitul dintr-un teasc.",
+    "O ve\u021bi putea m\u00e2nca \u00een orice loc, voi \u0219i casele voastre, c\u0103ci este plata voastr\u0103 pentru slujba voastr\u0103 \u00een Cortul \u00cent\u00e2lnirii.",
+    "\u015ei nu ve\u021bi purta niciun p\u0103cat din pricina ei, c\u00e2nd ve\u021bi fi adus ce este mai bun din ea; nu ve\u021bi spurca lucrurile sfinte ale fiilor lui Israel \u0219i nu ve\u021bi muri.\u00bb\u201d",
+  ],
+}
