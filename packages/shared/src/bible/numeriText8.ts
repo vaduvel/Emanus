68e98be8c159be17c4745157d0b421a9ebfb28b8
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:29:01 +0300

    Numeri 8: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText8.ts
@@ -0,0 +1,38 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 8.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.8.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 26 de versete.
+ */
+
+export const NUMERI_TEXT_8: Record<number, readonly string[]> = {
+  8: [
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eVorbeste-i lui Aaron \u0219i spune-i: C\u00e2nd vei a\u015feza candelele, cele \u0219apte candele trebuie s\u0103 lumineze \u00een fa\u021ba sfe\u015fnicului.\u201d",
+    "Aaron a f\u0103cut a\u015fa: a a\u015fezat candelele \u00een fa\u021ba sfe\u015fnicului, a\u015fa cum \u00eei poruncise DOMNUL lui Moise.",
+    "Lucr\u0103tura sfe\u015fnicului era din aur b\u0103tut: de la piciorul lui p\u00e2n\u0103 la florile lui era aur b\u0103tut. Dup\u0103 modelul pe care i-l ar\u0103tase DOMNUL lui Moise, a\u015fa a f\u0103cut sfe\u015fnicul.",
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eIa-i pe levi\u021bi din mijlocul fiilor lui Israel \u0219i cur\u0103\u021be\u015fte-i.",
+    "Iat\u0103 ce s\u0103 le faci ca s\u0103-i cure\u021bi: strope\u015fte-i cu ap\u0103 de cur\u0103\u021bire, ei s\u0103 fac\u0103 s\u0103 treac\u0103 briciul peste tot trupul lor, s\u0103-\u0219i spele hainele \u0219i s\u0103 se cure\u021beasc\u0103.",
+    "Apoi s\u0103 ia un taur t\u00e2n\u0103r cu ofranda lui de m\u00e2ncare din f\u0103in\u0103 aleas\u0103 fr\u0103m\u00e2ntat\u0103 cu ulei, iar tu s\u0103 iei un alt taur t\u00e2n\u0103r ca jertf\u0103 pentru p\u0103cat.",
+    "S\u0103-i apropii pe levi\u021bi \u00eenaintea Cortului \u00cent\u00e2lnirii \u0219i s\u0103 str\u00e2ngi toat\u0103 adunarea fiilor lui Israel.",
+    "S\u0103-i aduci pe levi\u021bi \u00eenaintea DOMNULUI, iar fiii lui Israel s\u0103-\u0219i pun\u0103 m\u00e2inile peste levi\u021bi.",
+    "Aaron s\u0103-i legene pe levi\u021bi ca o jertf\u0103 leg\u0103nat\u0103 \u00eenaintea DOMNULUI din partea fiilor lui Israel, ca ei s\u0103 fie gata s\u0103 fac\u0103 slujba DOMNULUI.",
+    "Levi\u021bii s\u0103-\u0219i pun\u0103 m\u00e2inile pe capul taurilor; iar tu s\u0103 aduci pe unul ca jertf\u0103 pentru p\u0103cat \u0219i pe cel\u0103lalt ca ardere-de-tot DOMNULUI, ca s\u0103 faci isp\u0103\u015fire pentru levi\u021bi.",
+    "S\u0103-i pui pe levi\u021bi s\u0103 stea \u00eenaintea lui Aaron \u0219i \u00eenaintea fiilor lui \u0219i s\u0103-i legeni ca jertf\u0103 leg\u0103nat\u0103 pentru DOMNUL.",
+    "A\u015fa s\u0103-i pui deoparte pe levi\u021bi din mijlocul fiilor lui Israel; iar levi\u021bii vor fi ai Mei.",
+    "Dup\u0103 aceea, levi\u021bii vor intra s\u0103 fac\u0103 slujba Cortului \u00cent\u00e2lnirii, dup\u0103 ce-i vei fi cur\u0103\u021bit \u0219i-i vei fi leg\u0103nat ca jertf\u0103 leg\u0103nat\u0103.",
+    "C\u0103ci ei \u00cemi sunt da\u021bi cu totul din mijlocul fiilor lui Israel; I-am luat pentru Mine \u00een locul tuturor \u00eent\u00e2ilor n\u0103scu\u021bi care deschid p\u00e2ntecele dintre to\u021bi fiii lui Israel.",
+    "C\u0103ci orice \u00eent\u00e2i n\u0103scut dintre fiii lui Israel este al Meu, at\u00e2t dintre oameni, c\u00e2t \u0219i dintre animale; \u00een ziua \u00een care i-am lovit pe to\u021bi \u00eent\u00e2ii n\u0103scu\u021bi \u00een \u021bara Egiptului, Mi-i sfin\u021bisem Mie.",
+    "\u015ei i-am luat pe levi\u021bi \u00een locul tuturor \u00eent\u00e2ilor n\u0103scu\u021bi dintre fiii lui Israel.",
+    "I-am dat pe levi\u021bi ca un dar lui Aaron \u0219i fiilor lui din mijlocul fiilor lui Israel, ca s\u0103 fac\u0103 slujba fiilor lui Israel \u00een Cortul \u00cent\u00e2lnirii \u0219i s\u0103 fac\u0103 isp\u0103\u015fire pentru fiii lui Israel, ca s\u0103 nu existe nicio urgie printre fiii lui Israel c\u00e2nd fiii lui Israel se vor apropia de Sf\u00e2ntul L\u0103ca\u015f.\u201d",
+    "Moise, Aaron \u0219i toat\u0103 adunarea fiilor lui Israel le-au f\u0103cut levi\u021bilor dup\u0103 tot ce-i poruncise DOMNUL lui Moise cu privire la levi\u021bi; a\u015fa le-au f\u0103cut fiii lui Israel.",
+    "Levi\u021bii s-au cur\u0103\u021bit de p\u0103cat \u0219i \u0219i-au sp\u0103lat hainele; Aaron i-a leg\u0103nat ca jertf\u0103 leg\u0103nat\u0103 \u00eenaintea DOMNULUI \u0219i Aaron a f\u0103cut isp\u0103\u015fire pentru ei ca s\u0103-i cure\u021beasc\u0103.",
+    "Dup\u0103 aceea, levi\u021bii au intrat s\u0103-\u0219i fac\u0103 slujba \u00een Cortul \u00cent\u00e2lnirii, \u00eenaintea lui Aaron \u0219i \u00eenaintea fiilor lui; cum \u00eei poruncise DOMNUL lui Moise cu privire la levi\u021bi, a\u015fa le-au f\u0103cut.",
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eAceasta este ce-i priveste pe levi\u021bi: de la v\u00e2rsta de dou\u0103zeci \u0219i cinci de ani \u00een sus, ei vor intra \u00een slujb\u0103 ca s\u0103 fac\u0103 lucrarea Cortului \u00cent\u00e2lnirii;",
+    "iar de la v\u00e2rsta de cincizeci de ani vor ie\u015fi din slujba lucr\u0103rii \u0219i nu vor mai sluji.",
+    "Vor putea ajuta pe fra\u021bii lor \u00een Cortul \u00cent\u00e2lnirii ca s\u0103 p\u0103zeasc\u0103 paza, dar nu vor mai face slujb\u0103 activ\u0103. A\u015fa s\u0103 faci cu levi\u021bii \u00een ce prive\u015fte \u00eens\u0103rcin\u0103rile lor.\u201d",
+  ],
+}
