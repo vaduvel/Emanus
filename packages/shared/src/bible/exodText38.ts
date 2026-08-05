c59c69b6178a31ec6870f4692f90e50c83e3c7db
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 09:58:50 +0300

    Exod 38: textul biblic din Biblia Emanus

--- packages/shared/src/bible/exodText38.ts
+++ packages/shared/src/bible/exodText38.ts
@@ -1,15 +1,43 @@
 /*
- * Textul biblic al cărții Exod, capitolul 38.
+ * Textul biblic al c\u0103r\u021bii Exodul, capitolul 38.
  *
- * ATENȚIE, agentul care lucrează la Biblia Emanus:
- * fișierul acesta ține LOCUL textului, nu textul. Fiecare intrare din tablou
- * este un verset, în ordine, de la 1 la 31. Înlocuiește fiecare șir cu versetul
- * din Biblia Emanus (validată din trei părți) și nu schimba nimic altceva:
- * nici numărul intrărilor, nici cheia capitolului, nici numele exportului.
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/EXO.38.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 31 de versete.
  */
 
-const PLACEHOLDER = "[textul biblic se completează din Biblia Emanus]"
-
 export const EXOD_TEXT_38: Record<number, readonly string[]> = {
-  38: Array.from({ length: 31 }, (_, i) => `[Exod 38:${i + 1}] ${PLACEHOLDER}`),
+  38: [
+    "A f\u0103cut altarul arderii-de-tot din lemn de salc\u00e2m, lung de cinci co\u021bi, lat de cinci co\u021bi, p\u0103trat, \u0219i \u00eenalt de trei co\u021bi.",
+    "I-a f\u0103cut coarne la cele patru col\u021buri; coarnele erau dintr-o singur\u0103 bucat\u0103 cu el. L-a acoperit cu bronz.",
+    "A f\u0103cut toate uneltele altarului: vasele pentru cenu\u0219\u0103, lope\u021bile, lighenele, furculi\u021bele \u0219i f\u0103ra\u0219ele pentru c\u0103rbuni. Toate uneltele lui le-a f\u0103cut din bronz.",
+    "A f\u0103cut pentru altar un gr\u0103tar ca o re\u021bea din bronz, sub marginea altarului, \u00een partea de jos, p\u00e2n\u0103 la jum\u0103tatea lui.",
+    "A turnat patru verigi pentru cele patru col\u021buri ale gr\u0103tarului de bronz, ca s\u0103 \u021bin\u0103 drugii.",
+    "A f\u0103cut drugii din lemn de salc\u00e2m \u0219i i-a acoperit cu bronz.",
+    "A introdus drugii \u00een verigile de pe laturile altarului, pentru a-l purta cu ei. A f\u0103cut altarul gol pe din\u0103untru, din sc\u00e2nduri.",
+    "A f\u0103cut ligheanul \u0219i suportul lui din bronz, folosind oglinzile femeilor care slujeau la intrarea Cortului \u00cent\u00e2lnirii.",
+    "A f\u0103cut curtea. Pe latura de sud, p\u00e2nzele cur\u021bii din in fin r\u0103sucit aveau o sut\u0103 de co\u021bi.",
+    "Erau dou\u0103zeci de st\u00e2lpi cu dou\u0103zeci de socluri din bronz; c\u00e2rligele \u0219i leg\u0103turile st\u00e2lpilor erau din argint.",
+    "Pe latura de nord, p\u00e2nzele aveau o sut\u0103 de co\u021bi, cu dou\u0103zeci de st\u00e2lpi \u0219i dou\u0103zeci de socluri din bronz; c\u00e2rligele \u0219i leg\u0103turile st\u00e2lpilor erau din argint.",
+    "Pe latura de apus, p\u00e2nzele aveau cincizeci de co\u021bi, cu zece st\u00e2lpi \u0219i zece socluri; c\u00e2rligele \u0219i leg\u0103turile st\u00e2lpilor erau din argint.",
+    "Latura de r\u0103s\u0103rit avea cincizeci de co\u021bi.",
+    "Pe o parte a intr\u0103rii erau p\u00e2nze de cincisprezece co\u021bi, cu trei st\u00e2lpi \u0219i trei socluri.",
+    "La fel era pe cealalt\u0103 parte; de o parte \u0219i de alta a intr\u0103rii cur\u021bii erau p\u00e2nze de cincisprezece co\u021bi, cu trei st\u00e2lpi \u0219i trei socluri.",
+    "Toate p\u00e2nzele din jurul cur\u021bii erau din in fin r\u0103sucit.",
+    "Soclurile st\u00e2lpilor erau din bronz, iar c\u00e2rligele \u0219i leg\u0103turile st\u00e2lpilor din argint. Capitelurile lor erau acoperite cu argint \u0219i to\u021bi st\u00e2lpii cur\u021bii erau lega\u021bi cu argint.",
+    "Paravanul de la intrarea cur\u021bii era lucrat prin broderie, din fir albastru, purpuriu \u0219i stacojiu \u0219i din in fin r\u0103sucit. Era lung de dou\u0103zeci de co\u021bi \u0219i \u00eenalt de cinci co\u021bi, asemenea p\u00e2nzelor cur\u021bii.",
+    "Avea patru st\u00e2lpi cu patru socluri din bronz; c\u00e2rligele, \u00eenveli\u0219ul capitelurilor \u0219i leg\u0103turile lor erau din argint.",
+    "To\u021bi \u021b\u0103ru\u0219ii Cortului \u0219i ai cur\u021bii din jur erau din bronz.",
+    "Acestea sunt cantit\u0103\u021bile materialelor folosite pentru Cort, Cortul M\u0103rturiei, num\u0103rate dup\u0103 porunca lui Moise pentru slujirea levi\u021bilor, sub conducerea lui Itamar, fiul preotului Aaron.",
+    "Be\u021baleel, fiul lui Uri, fiul lui Hur, din semin\u021bia lui Iuda, a f\u0103cut tot ce \u00eei poruncise DOMNUL lui Moise.",
+    "\u00cempreun\u0103 cu el a lucrat Oholiab, fiul lui Ahisamac, din semin\u021bia lui Dan, gravor, artist \u0219i broder \u00een fir albastru, purpuriu \u0219i stacojiu \u0219i \u00een in fin.",
+    "Tot aurul folosit pentru \u00eentreaga lucrare a sanctuarului, aurul contribu\u021biei, a fost de dou\u0103zeci \u0219i nou\u0103 de talan\u021bi \u0219i \u0219apte sute treizeci de \u0219echeli, dup\u0103 \u0219echelul sanctuarului.",
+    "Argintul celor num\u0103ra\u021bi din comunitate a fost de o sut\u0103 de talan\u021bi \u0219i o mie \u0219apte sute \u0219aptezeci \u0219i cinci de \u0219echeli, dup\u0103 \u0219echelul sanctuarului:",
+    "c\u00e2te o bec\u0103 de persoan\u0103, adic\u0103 o jum\u0103tate de \u0219echel dup\u0103 \u0219echelul sanctuarului, pentru fiecare dintre cei num\u0103ra\u021bi de la v\u00e2rsta de dou\u0103zeci de ani \u00een sus, \u00een total \u0219ase sute trei mii cinci sute cincizeci de b\u0103rba\u021bi.",
+    "Cei o sut\u0103 de talan\u021bi de argint au fost folosi\u021bi pentru turnarea soclurilor sanctuarului \u0219i a soclurilor perdelei: o sut\u0103 de socluri din o sut\u0103 de talan\u021bi, c\u00e2te un talant pentru fiecare soclu.",
+    "Din cei o mie \u0219apte sute \u0219aptezeci \u0219i cinci de \u0219echeli a f\u0103cut c\u00e2rligele st\u00e2lpilor, le-a acoperit capitelurile \u0219i le-a f\u0103cut leg\u0103turile.",
+    "Bronzul contribu\u021biei a fost de \u0219aptezeci de talan\u021bi \u0219i dou\u0103 mii patru sute de \u0219echeli.",
+    "Cu el a f\u0103cut soclurile de la intrarea Cortului \u00cent\u00e2lnirii, altarul de bronz, gr\u0103tarul lui de bronz, toate uneltele altarului,",
+    "soclurile din jurul cur\u021bii, soclurile de la intrarea cur\u021bii, to\u021bi \u021b\u0103ru\u0219ii Cortului \u0219i to\u021bi \u021b\u0103ru\u0219ii din jurul cur\u021bii.",
+  ],
 }
