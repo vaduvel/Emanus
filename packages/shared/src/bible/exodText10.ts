f8af6e0d387ec29eb057d4cfcf5c39122d3bd985
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 09:12:33 +0300

    Exod 10: textul biblic din Biblia Emanus

--- packages/shared/src/bible/exodText10.ts
+++ packages/shared/src/bible/exodText10.ts
@@ -1,15 +1,41 @@
 /*
- * Textul biblic al cărții Exod, capitolul 10.
+ * Textul biblic al c\u0103r\u021bii Exod, capitolul 10.
  *
- * ATENȚIE, agentul care lucrează la Biblia Emanus:
- * fișierul acesta ține LOCUL textului, nu textul. Fiecare intrare din tablou
- * este un verset, în ordine, de la 1 la 29. Înlocuiește fiecare șir cu versetul
- * din Biblia Emanus (validată din trei părți) și nu schimba nimic altceva:
- * nici numărul intrărilor, nici cheia capitolului, nici numele exportului.
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/EXO.10.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 29 de versete.
  */
 
-const PLACEHOLDER = "[textul biblic se completează din Biblia Emanus]"
-
 export const EXOD_TEXT_10: Record<number, readonly string[]> = {
-  10: Array.from({ length: 29 }, (_, i) => `[Exod 10:${i + 1}] ${PLACEHOLDER}`),
+  10: [
+    "DOMNUL i-a spus lui Moise: \u201eDu-te la faraon, c\u0103ci i-am \u00eempietrit inima lui \u0219i inimile slujitorilor lui, ca s\u0103 pun aceste semne ale Mele \u00een mijlocul lor",
+    "\u0219i ca tu s\u0103 poveste\u0219ti fiului t\u0103u \u0219i fiului fiului t\u0103u cum am lucrat \u00eempotriva Egiptului \u0219i ce semne am f\u0103cut \u00een mijlocul lor, ca s\u0103 \u0219ti\u021bi c\u0103 Eu sunt DOMNUL.\u201d",
+    "Moise \u0219i Aaron au intrat la faraon \u0219i i-au spus: \u201eA\u0219a vorbe\u0219te DOMNUL, Dumnezeul evreilor: \u00abP\u00e2n\u0103 c\u00e2nd vei refuza s\u0103 te smere\u0219ti \u00eenaintea Mea? Las\u0103 poporul Meu s\u0103 plece ca s\u0103-Mi slujeasc\u0103.",
+    "Dac\u0103 refuzi s\u0103 la\u0219i poporul Meu s\u0103 plece, iat\u0103, m\u00e2ine voi aduce l\u0103custe \u00een \u021binutul t\u0103u.",
+    "Ele vor acoperi fa\u021ba p\u0103m\u00e2ntului, astfel \u00eenc\u00e2t p\u0103m\u00e2ntul nu va mai putea fi v\u0103zut. Vor m\u00e2nca ce a sc\u0103pat \u0219i v-a r\u0103mas dup\u0103 grindin\u0103 \u0219i vor m\u00e2nca orice copac care cre\u0219te pe c\u00e2mpurile voastre.",
+    "Casele tale, casele tuturor slujitorilor t\u0103i \u0219i casele tuturor egiptenilor se vor umple de ele, cum nu au v\u0103zut p\u0103rin\u021bii \u0219i str\u0103mo\u0219ii t\u0103i din ziua \u00een care au fost pe p\u0103m\u00e2nt \u0219i p\u00e2n\u0103 ast\u0103zi.\u00bb\u201d Apoi Moise s-a \u00eentors \u0219i a ie\u0219it de la faraon.",
+    "Slujitorii faraonului i-au spus: \u201eP\u00e2n\u0103 c\u00e2nd va fi omul acesta o curs\u0103 pentru noi? Las\u0103-i pe b\u0103rba\u021bi s\u0103 plece ca s\u0103-I slujeasc\u0103 DOMNULUI, Dumnezeul lor. \u00cenc\u0103 nu \u00een\u021belegi c\u0103 Egiptul este distrus?\u201d",
+    "Moise \u0219i Aaron au fost adu\u0219i din nou la faraon, iar el le-a spus: \u201eDuce\u021bi-v\u0103 \u0219i sluji\u021bi-I DOMNULUI, Dumnezeul vostru. Dar cine anume va merge?\u201d",
+    "Moise a r\u0103spuns: \u201eVom merge cu tinerii \u0219i cu b\u0103tr\u00e2nii no\u0219tri, cu fiii \u0219i cu fiicele noastre, cu turmele \u0219i cu cirezile noastre, fiindc\u0103 avem de \u021binut o s\u0103rb\u0103toare pentru DOMNUL.\u201d",
+    "Faraonul le-a spus: \u201eA\u0219a s\u0103 fie DOMNUL cu voi cum v\u0103 voi l\u0103sa eu s\u0103 pleca\u021bi \u00eempreun\u0103 cu copiii vo\u0219tri! Este limpede c\u0103 ave\u021bi inten\u021bii rele.",
+    "Nu! Merge\u021bi acum numai voi, b\u0103rba\u021bii, \u0219i sluji\u021bi-I DOMNULUI, fiindc\u0103 aceasta a\u021bi cerut.\u201d Apoi au fost izgoni\u021bi dinaintea faraonului.",
+    "DOMNUL i-a spus lui Moise: \u201e\u00centinde-\u021bi m\u00e2na peste \u021bara Egiptului ca s\u0103 vin\u0103 l\u0103custele peste ea \u0219i s\u0103 m\u0103n\u00e2nce toate plantele \u021b\u0103rii, tot ce a l\u0103sat grindina.\u201d",
+    "Moise \u0219i-a \u00eentins toiagul peste \u021bara Egiptului, iar DOMNUL a adus peste \u021bar\u0103 un v\u00e2nt de r\u0103s\u0103rit toat\u0103 ziua aceea \u0219i toat\u0103 noaptea. Diminea\u021ba, v\u00e2ntul de r\u0103s\u0103rit adusese l\u0103custele.",
+    "L\u0103custele s-au ridicat peste toat\u0103 \u021bara Egiptului \u0219i s-au a\u0219ezat \u00een tot \u021binutul Egiptului. Erau nespus de multe; \u00eenaintea lor nu mai fuseser\u0103 asemenea l\u0103custe \u0219i nici dup\u0103 ele nu vor mai fi.",
+    "Au acoperit fa\u021ba \u00eentregului p\u0103m\u00e2nt, \u00eenc\u00e2t \u021bara s-a \u00eentunecat. Au m\u00e2ncat toate plantele \u021b\u0103rii \u0219i toate roadele copacilor pe care le l\u0103sase grindina. Nu a mai r\u0103mas nimic verde, nici copac, nici plant\u0103 a c\u00e2mpului, \u00een toat\u0103 \u021bara Egiptului.",
+    "Faraonul i-a chemat \u00een grab\u0103 pe Moise \u0219i pe Aaron \u0219i le-a spus: \u201eAm p\u0103c\u0103tuit \u00eempotriva DOMNULUI, Dumnezeul vostru, \u0219i \u00eempotriva voastr\u0103.",
+    "Acum, te rog, iart\u0103-mi p\u0103catul \u00eenc\u0103 o dat\u0103 \u0219i ruga\u021bi-L pe DOMNUL, Dumnezeul vostru, s\u0103 \u00eendep\u0103rteze de la mine aceast\u0103 moarte.\u201d",
+    "Moise a ie\u0219it de la faraon \u0219i s-a rugat DOMNULUI.",
+    "DOMNUL a f\u0103cut s\u0103 sufle un v\u00e2nt foarte puternic dinspre apus, care a ridicat l\u0103custele \u0219i le-a aruncat \u00een Marea Ro\u0219ie. Nu a mai r\u0103mas nicio l\u0103cust\u0103 \u00een tot \u021binutul Egiptului.",
+    "Dar DOMNUL a \u00eempietrit inima faraonului, iar el nu i-a l\u0103sat pe fiii lui Israel s\u0103 plece.",
+    "DOMNUL i-a spus lui Moise: \u201e\u00centinde-\u021bi m\u00e2na spre cer, ca peste \u021bara Egiptului s\u0103 vin\u0103 un \u00eentuneric at\u00e2t de gros, \u00eenc\u00e2t s\u0103 poat\u0103 fi pip\u0103it.\u201d",
+    "Moise \u0219i-a \u00eentins m\u00e2na spre cer, iar peste toat\u0103 \u021bara Egiptului a fost \u00eentuneric gros timp de trei zile.",
+    "Oamenii nu se vedeau unii pe al\u021bii \u0219i nimeni nu s-a ridicat din locul s\u0103u timp de trei zile. Dar to\u021bi fiii lui Israel aveau lumin\u0103 \u00een locuin\u021bele lor.",
+    "Faraonul l-a chemat pe Moise \u0219i i-a spus: \u201eDuce\u021bi-v\u0103 \u0219i sluji\u021bi-I DOMNULUI! Numai turmele \u0219i cirezile voastre s\u0103 r\u0103m\u00e2n\u0103; chiar \u0219i copiii vo\u0219tri pot merge cu voi.\u201d",
+    "Moise a r\u0103spuns: \u201eTu \u00eensu\u021bi trebuie s\u0103 ne dai animale pentru jertfe \u0219i arderi-de-tot, ca s\u0103 le aducem DOMNULUI, Dumnezeul nostru.",
+    "\u0218i animalele noastre vor merge cu noi; nu va r\u0103m\u00e2ne nici m\u0103car o copit\u0103, fiindc\u0103 din ele vom lua ca s\u0103-I slujim DOMNULUI, Dumnezeul nostru. P\u00e2n\u0103 nu ajungem acolo, nu \u0219tim cu ce \u00cei vom sluji DOMNULUI.\u201d",
+    "Dar DOMNUL a \u00eempietrit inima faraonului \u0219i el nu a vrut s\u0103-i lase s\u0103 plece.",
+    "Faraonul i-a spus: \u201ePleac\u0103 de la mine! Ai grij\u0103 s\u0103 nu-mi mai vezi fa\u021ba, c\u0103ci \u00een ziua \u00een care \u00eemi vei vedea fa\u021ba vei muri!\u201d",
+    "Moise a r\u0103spuns: \u201eAi vorbit bine. Nu-\u021bi voi mai vedea fa\u021ba.\u201d",
+  ],
 }
