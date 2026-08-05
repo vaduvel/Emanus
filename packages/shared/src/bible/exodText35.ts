0dfd707a8f2e34ae2642ca2ce7f24d2defcaec93
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 09:55:07 +0300

    Exod 35: textul biblic din Biblia Emanus

--- packages/shared/src/bible/exodText35.ts
+++ packages/shared/src/bible/exodText35.ts
@@ -1,15 +1,47 @@
 /*
- * Textul biblic al cărții Exod, capitolul 35.
+ * Textul biblic al c\u0103r\u021bii Exodul, capitolul 35.
  *
- * ATENȚIE, agentul care lucrează la Biblia Emanus:
- * fișierul acesta ține LOCUL textului, nu textul. Fiecare intrare din tablou
- * este un verset, în ordine, de la 1 la 35. Înlocuiește fiecare șir cu versetul
- * din Biblia Emanus (validată din trei părți) și nu schimba nimic altceva:
- * nici numărul intrărilor, nici cheia capitolului, nici numele exportului.
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/EXO.35.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 35 de versete.
  */
 
-const PLACEHOLDER = "[textul biblic se completează din Biblia Emanus]"
-
 export const EXOD_TEXT_35: Record<number, readonly string[]> = {
-  35: Array.from({ length: 35 }, (_, i) => `[Exod 35:${i + 1}] ${PLACEHOLDER}`),
+  35: [
+    "Moise a adunat \u00eentreaga comunitate a fiilor lui Israel \u0219i le-a spus: \u201eAcestea sunt lucrurile pe care DOMNUL a poruncit s\u0103 le face\u021bi:",
+    "\u0218ase zile s\u0103 se lucreze, dar ziua a \u0219aptea s\u0103 fie pentru voi o zi sf\u00e2nt\u0103, un Sabat de odihn\u0103 deplin\u0103 pentru DOMNUL. Oricine face munc\u0103 \u00een ziua aceea s\u0103 fie omor\u00e2t.",
+    "S\u0103 nu aprinde\u021bi foc \u00een niciuna dintre locuin\u021bele voastre \u00een ziua Sabatului.\u201d",
+    "Moise a vorbit \u00eentregii comunit\u0103\u021bi a fiilor lui Israel \u0219i a spus: \u201eIat\u0103 ce a poruncit DOMNUL:",
+    "Lua\u021bi dintre bunurile voastre o contribu\u021bie pentru DOMNUL. Oricine are inima binevoitoare s\u0103 aduc\u0103 drept contribu\u021bie pentru DOMNUL aur, argint \u0219i bronz,",
+    "fir albastru, purpuriu \u0219i stacojiu, in fin, p\u0103r de capr\u0103,",
+    "piei de berbec vopsite \u00een ro\u0219u, piei fine \u0219i lemn de salc\u00e2m,",
+    "ulei pentru lumin\u0103, mirodenii pentru untdelemnul ungerii \u0219i pentru t\u0103m\u00e2ia parfumat\u0103,",
+    "pietre de onix \u0219i pietre de montat pentru efod \u0219i pieptar.",
+    "To\u021bi cei pricepu\u021bi dintre voi s\u0103 vin\u0103 \u0219i s\u0103 fac\u0103 tot ce a poruncit DOMNUL:",
+    "Cortul, acoper\u0103m\u00e2ntul \u0219i \u00eenveli\u0219ul lui, copcile, sc\u00e2ndurile, drugii, st\u00e2lpii \u0219i soclurile lui;",
+    "chivotul cu drugii lui, capacul isp\u0103\u0219irii \u0219i perdeaua desp\u0103r\u021bitoare;",
+    "masa cu drugii \u0219i toate obiectele ei \u0219i p\u00e2inea Prezen\u021bei;",
+    "sfe\u0219nicul pentru lumin\u0103 cu obiectele, candelele \u0219i uleiul lui;",
+    "altarul t\u0103m\u00e2ierii cu drugii lui, untdelemnul ungerii, t\u0103m\u00e2ia parfumat\u0103 \u0219i paravanul de la intrarea Cortului;",
+    "altarul arderii-de-tot cu gr\u0103tarul lui de bronz, drugii \u0219i toate uneltele lui, ligheanul \u0219i suportul lui;",
+    "p\u00e2nzele cur\u021bii, st\u00e2lpii, soclurile \u0219i paravanul de la intrarea cur\u021bii;",
+    "\u021b\u0103ru\u0219ii Cortului, \u021b\u0103ru\u0219ii cur\u021bii \u0219i funiile lor;",
+    "ve\u0219mintele lucrate pentru slujirea \u00een Locul Sf\u00e2nt, ve\u0219mintele sfinte ale preotului Aaron \u0219i ve\u0219mintele fiilor lui pentru slujirea preo\u021beasc\u0103.\u201d",
+    "\u00centreaga comunitate a fiilor lui Israel a plecat dinaintea lui Moise.",
+    "Apoi au venit to\u021bi cei a c\u0103ror inim\u0103 \u00eei \u00eendemna \u0219i al c\u0103ror duh \u00eei f\u0103cea binevoitori \u0219i au adus contribu\u021bia DOMNULUI pentru lucrarea Cortului \u00cent\u00e2lnirii, pentru \u00eentreaga lui slujire \u0219i pentru ve\u0219mintele sfinte.",
+    "Au venit at\u00e2t b\u0103rba\u021bi, c\u00e2t \u0219i femei, to\u021bi cei cu inim\u0103 binevoitoare, \u0219i au adus bro\u0219e, cercei, inele, br\u0103\u021b\u0103ri \u0219i tot felul de obiecte de aur. Fiecare a adus un dar de aur pentru DOMNUL.",
+    "Oricine avea fir albastru, purpuriu sau stacojiu, in fin, p\u0103r de capr\u0103, piei de berbec vopsite \u00een ro\u0219u sau piei fine le-a adus.",
+    "Oricine aducea o contribu\u021bie de argint sau de bronz a adus-o ca dar pentru DOMNUL, iar oricine avea lemn de salc\u00e2m potrivit pentru vreo lucrare a slujirii l-a adus.",
+    "Toate femeile pricepute au tors cu m\u00e2inile lor \u0219i au adus ce torseser\u0103: fir albastru, purpuriu \u0219i stacojiu \u0219i in fin.",
+    "Toate femeile a c\u0103ror inim\u0103 le \u00eendemna prin pricepere au tors p\u0103r de capr\u0103.",
+    "Conduc\u0103torii au adus pietrele de onix, pietrele de montat pentru efod \u0219i pieptar,",
+    "mirodeniile \u0219i uleiul pentru lumin\u0103, pentru untdelemnul ungerii \u0219i pentru t\u0103m\u00e2ia parfumat\u0103.",
+    "To\u021bi b\u0103rba\u021bii \u0219i toate femeile dintre fiii lui Israel a c\u0103ror inim\u0103 \u00eei f\u0103cea binevoitori s\u0103 aduc\u0103 pentru \u00eentreaga lucrare pe care DOMNUL poruncise prin Moise s\u0103 fie f\u0103cut\u0103 au adus DOMNULUI un dar de bun\u0103voie.",
+    "Moise le-a spus fiilor lui Israel: \u201eVede\u021bi, DOMNUL l-a chemat pe nume pe Be\u021baleel, fiul lui Uri, fiul lui Hur, din semin\u021bia lui Iuda.",
+    "L-a umplut cu Duhul lui Dumnezeu, cu \u00een\u021belepciune, pricepere, cunoa\u0219tere \u0219i \u00eendem\u00e2nare \u00een orice me\u0219te\u0219ug,",
+    "ca s\u0103 conceap\u0103 lucr\u0103ri artistice \u0219i s\u0103 lucreze \u00een aur, argint \u0219i bronz,",
+    "s\u0103 taie pietre pentru montare, s\u0103 cioplesc\u0103 lemnul \u0219i s\u0103 lucreze \u00een orice me\u0219te\u0219ug artistic.",
+    "Dumnezeu a pus \u00een inima lui \u0219i \u00een a lui Oholiab, fiul lui Ahisamac, din semin\u021bia lui Dan, capacitatea de a-i \u00eenv\u0103\u021ba \u0219i pe al\u021bii.",
+    "I-a umplut cu pricepere la inim\u0103 pentru a face orice lucrare de gravor, artist, broder \u00een fir albastru, purpuriu \u0219i stacojiu \u0219i \u00een in fin, \u021bes\u0103tor \u0219i creator de lucr\u0103ri artistice.\u201d",
+  ],
 }
