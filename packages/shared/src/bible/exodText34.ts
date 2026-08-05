5b7c18edd121ae8fa783d0b046bf99e08587e2cd
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 09:45:41 +0300

    Exod 34: textul biblic din Biblia Emanus

--- packages/shared/src/bible/exodText34.ts
+++ packages/shared/src/bible/exodText34.ts
@@ -1,15 +1,47 @@
 /*
- * Textul biblic al cărții Exod, capitolul 34.
+ * Textul biblic al c\u0103r\u021bii Exod, capitolul 34.
  *
- * ATENȚIE, agentul care lucrează la Biblia Emanus:
- * fișierul acesta ține LOCUL textului, nu textul. Fiecare intrare din tablou
- * este un verset, în ordine, de la 1 la 35. Înlocuiește fiecare șir cu versetul
- * din Biblia Emanus (validată din trei părți) și nu schimba nimic altceva:
- * nici numărul intrărilor, nici cheia capitolului, nici numele exportului.
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/EXO.34.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 35 de versete.
  */
 
-const PLACEHOLDER = "[textul biblic se completează din Biblia Emanus]"
-
 export const EXOD_TEXT_34: Record<number, readonly string[]> = {
-  34: Array.from({ length: 35 }, (_, i) => `[Exod 34:${i + 1}] ${PLACEHOLDER}`),
+  34: [
+    "DOMNUL i-a spus lui Moise: \u201eCiople\u0219te dou\u0103 table de piatr\u0103 asemenea celor dint\u00e2i. Eu voi scrie pe table cuvintele care erau pe primele table, pe care le-ai sf\u0103r\u00e2mat.",
+    "Fii gata diminea\u021b\u0103. Urc\u0103 diminea\u021b\u0103 pe muntele Sinai \u0219i \u00eenf\u0103\u021bi\u0219eaz\u0103-te \u00eenaintea Mea acolo, pe v\u00e2rful muntelui.",
+    "Nimeni s\u0103 nu urce \u00eempreun\u0103 cu tine \u0219i nimeni s\u0103 nu fie v\u0103zut pe tot muntele. Nici turmele, nici cirezile s\u0103 nu pasc\u0103 \u00eenaintea muntelui.\u201d",
+    "Moise a cioplit dou\u0103 table de piatr\u0103 asemenea celor dint\u00e2i. S-a sculat dis-de-diminea\u021b\u0103 \u0219i s-a urcat pe muntele Sinai, a\u0219a cum \u00eei poruncise DOMNUL, av\u00e2nd \u00een m\u00e2n\u0103 cele dou\u0103 table de piatr\u0103.",
+    "DOMNUL a cobor\u00e2t \u00een nor, a stat acolo \u00eempreun\u0103 cu el \u0219i a proclamat Numele DOMNULUI.",
+    "DOMNUL a trecut pe dinaintea lui \u0219i a proclamat: \u201eDOMNUL! DOMNUL! Dumnezeu milos \u0219i plin de har, \u00eencet la m\u00e2nie, bogat \u00een bun\u0103tate statornic\u0103 \u0219i credincio\u0219ie,",
+    "Care p\u0103streaz\u0103 bun\u0103tatea statornic\u0103 pentru mii, iart\u0103 nelegiuirea, r\u0103zvr\u0103tirea \u0219i p\u0103catul, dar nu-l declar\u0103 nevinovat pe cel vinovat; cerceteaz\u0103 vina p\u0103rin\u021bilor \u00een copii \u0219i \u00een copiii copiilor, p\u00e2n\u0103 la a treia \u0219i a patra genera\u021bie.\u201d",
+    "Moise s-a gr\u0103bit, s-a plecat cu fa\u021ba la p\u0103m\u00e2nt \u0219i s-a \u00eenchinat.",
+    "El a spus: \u201eSt\u0103p\u00e2ne, dac\u0103 am g\u0103sit bun\u0103voin\u021b\u0103 \u00een ochii T\u0103i, Te rog s\u0103 mergi \u00een mijlocul nostru, chiar dac\u0103 este un popor cu g\u00e2tul \u00een\u021bepenit. Iart\u0103 nelegiuirea \u0219i p\u0103catul nostru \u0219i ia-ne ca mo\u0219tenire a Ta.\u201d",
+    "DOMNUL a r\u0103spuns: \u201eIat\u0103, \u00eenchei un leg\u0103m\u00e2nt. \u00cenaintea \u00eentregului t\u0103u popor voi face minuni cum nu s-au mai f\u0103cut \u00een tot p\u0103m\u00e2ntul \u0219i \u00een nicio na\u021biune. Tot poporul \u00een mijlocul c\u0103ruia te afli va vedea lucrarea DOMNULUI, c\u0103ci \u00eenfrico\u0219\u0103tor este ceea ce voi face cu tine.",
+    "P\u0103ze\u0219te ceea ce \u00ee\u021bi poruncesc ast\u0103zi. Iat\u0103, \u00eei voi izgoni dinaintea ta pe amori\u021bi, canaani\u021bi, heti\u021bi, perizi\u021bi, hevi\u021bi \u0219i iebusi\u021bi.",
+    "Ai grij\u0103 s\u0103 nu \u00eenchei leg\u0103m\u00e2nt cu locuitorii \u021b\u0103rii \u00een care mergi, ca s\u0103 nu devin\u0103 o curs\u0103 \u00een mijlocul t\u0103u.",
+    "Dimpotriv\u0103, s\u0103 le d\u0103r\u00e2ma\u021bi altarele, s\u0103 le sf\u0103r\u00e2ma\u021bi st\u00e2lpii sacri \u0219i s\u0103 le t\u0103ia\u021bi a\u0219erele.",
+    "S\u0103 nu te \u00eenchini altui dumnezeu, c\u0103ci DOMNUL, al C\u0103rui Nume este Gelos, este un Dumnezeu gelos.",
+    "S\u0103 nu \u00eenchei leg\u0103m\u00e2nt cu locuitorii \u021b\u0103rii, ca nu cumva, prostitu\u00e2ndu-se dup\u0103 dumnezeii lor \u0219i aduc\u00e2nd jertfe dumnezeilor lor, cineva s\u0103 te invite \u0219i tu s\u0103 m\u0103n\u00e2nci din jertfa lui.",
+    "S\u0103 nu iei dintre fiicele lor pentru fiii t\u0103i, ca nu cumva fiicele lor, prostitu\u00e2ndu-se dup\u0103 dumnezeii lor, s\u0103-i fac\u0103 \u0219i pe fiii t\u0103i s\u0103 se prostitueze dup\u0103 dumnezeii lor.",
+    "S\u0103 nu-\u021bi faci dumnezei turna\u021bi.",
+    "S\u0103 p\u0103ze\u0219ti S\u0103rb\u0103toarea Azimelor. Timp de \u0219apte zile s\u0103 m\u0103n\u00e2nci azime, a\u0219a cum \u021bi-am poruncit, la timpul hot\u0103r\u00e2t \u00een luna Abib, c\u0103ci \u00een luna Abib ai ie\u0219it din Egipt.",
+    "Orice \u00eent\u00e2i venit care deschide p\u00e2ntecele este al Meu, precum \u0219i orice \u00eent\u00e2i n\u0103scut de parte b\u0103rb\u0103teasc\u0103 al vitelor \u0219i oilor tale.",
+    "S\u0103 r\u0103scumperi \u00eent\u00e2iul n\u0103scut al m\u0103g\u0103ri\u021bei cu un animal din turm\u0103, iar dac\u0103 nu-l r\u0103scumperi, s\u0103-i fr\u00e2ngi g\u00e2tul. S\u0103 r\u0103scumperi orice \u00eent\u00e2i n\u0103scut dintre fiii t\u0103i. Nimeni s\u0103 nu se \u00eenf\u0103\u021bi\u0219eze \u00eenaintea Mea cu m\u00e2na goal\u0103.",
+    "\u0218ase zile s\u0103 lucrezi, dar \u00een ziua a \u0219aptea s\u0103 te odihne\u0219ti; chiar \u0219i \u00een timpul aratului \u0219i al seceri\u0219ului s\u0103 te odihne\u0219ti.",
+    "S\u0103 p\u0103ze\u0219ti S\u0103rb\u0103toarea S\u0103pt\u0103m\u00e2nilor, cu primele roade ale seceri\u0219ului gr\u00e2ului, \u0219i S\u0103rb\u0103toarea Str\u00e2ngerii Roadelor la \u00eencheierea anului.",
+    "De trei ori pe an, to\u021bi b\u0103rba\u021bii t\u0103i s\u0103 se \u00eenf\u0103\u021bi\u0219eze \u00eenaintea St\u0103p\u00e2nului, DOMNUL, Dumnezeul lui Israel.",
+    "C\u0103ci voi izgoni na\u021biunile dinaintea ta \u0219i \u00ee\u021bi voi l\u0103rgi hotarele. Nimeni nu-\u021bi va pofti \u021bara c\u00e2nd te vei urca s\u0103 te \u00eenf\u0103\u021bi\u0219ezi \u00eenaintea DOMNULUI, Dumnezeul t\u0103u, de trei ori pe an.",
+    "S\u0103 nu aduci s\u00e2ngele jertfei Mele \u00eempreun\u0103 cu p\u00e2ine dospit\u0103, iar jertfa S\u0103rb\u0103torii Pa\u0219telui s\u0103 nu r\u0103m\u00e2n\u0103 p\u00e2n\u0103 diminea\u021b\u0103.",
+    "S\u0103 aduci cele dint\u00e2i dintre primele roade ale p\u0103m\u00e2ntului t\u0103u \u00een Casa DOMNULUI, Dumnezeul t\u0103u. S\u0103 nu fierbi un ied \u00een laptele mamei sale.\u201d",
+    "DOMNUL i-a spus lui Moise: \u201eScrie aceste cuvinte, c\u0103ci pe temeiul acestor cuvinte am \u00eencheiat un leg\u0103m\u00e2nt cu tine \u0219i cu Israel.\u201d",
+    "Moise a r\u0103mas acolo cu DOMNUL patruzeci de zile \u0219i patruzeci de nop\u021bi. Nu a m\u00e2ncat p\u00e2ine \u0219i nu a b\u0103ut ap\u0103. A scris pe table cuvintele leg\u0103m\u00e2ntului, Cele Zece Cuvinte.",
+    "C\u00e2nd Moise a cobor\u00e2t de pe muntele Sinai, av\u00e2nd \u00een m\u00e2n\u0103 cele dou\u0103 table ale M\u0103rturiei, nu \u0219tia c\u0103 pielea fe\u021bei lui str\u0103lucea pentru c\u0103 vorbise cu Dumnezeu.",
+    "Aaron \u0219i to\u021bi fiii lui Israel l-au v\u0103zut pe Moise \u0219i iat\u0103 c\u0103 pielea fe\u021bei lui str\u0103lucea, iar ei s-au temut s\u0103 se apropie de el.",
+    "Moise i-a chemat, iar Aaron \u0219i toate c\u0103peteniile comunit\u0103\u021bii s-au \u00eentors la el. Moise le-a vorbit.",
+    "Dup\u0103 aceea s-au apropiat to\u021bi fiii lui Israel, iar el le-a poruncit tot ce \u00eei spusese DOMNUL pe muntele Sinai.",
+    "C\u00e2nd Moise a terminat de vorbit cu ei, \u0219i-a pus o \u00eenvelitoare pe fa\u021b\u0103.",
+    "Ori de c\u00e2te ori Moise intra \u00eenaintea DOMNULUI ca s\u0103 vorbeasc\u0103 cu El, \u00ee\u0219i scotea \u00eenvelitoarea p\u00e2n\u0103 c\u00e2nd ie\u0219ea. C\u00e2nd ie\u0219ea, le spunea fiilor lui Israel ce \u00eei fusese poruncit.",
+    "Fiii lui Israel vedeau c\u0103 pielea fe\u021bei lui Moise str\u0103lucea. Atunci Moise \u00ee\u0219i punea din nou \u00eenvelitoarea pe fa\u021b\u0103 p\u00e2n\u0103 c\u00e2nd intra s\u0103 vorbeasc\u0103 cu Dumnezeu.",
+  ],
 }
