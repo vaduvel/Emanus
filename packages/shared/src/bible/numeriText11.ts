e48136c2cb680bfa9e727f7fdf2de0663a119705
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:33:19 +0300

    Numeri 11: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText11.ts
@@ -0,0 +1,47 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 11.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.11.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 35 de versete.
+ */
+
+export const NUMERI_TEXT_11: Record<number, readonly string[]> = {
+  11: [
+    "Poporul a \u00eenceput s\u0103 c\u00e2rteasc\u0103 amar la urechile DOMNULUI. DOMNUL a auzit \u0219i m\u00e2nia Lui S-a aprins; focul DOMNULUI s-a aprins printre ei \u0219i a mistuit o parte din marginea taberei.",
+    "Poporul a strigat c\u0103tre Moise; Moise s-a rugat DOMNULUI \u0219i focul s-a potolit.",
+    "Locului aceluia i-au pus numele Tabera, pentru c\u0103 focul DOMNULUI se aprinsese printre ei.",
+    "Adul\u0103tura de oameni care era \u00een mijlocul lor a fost apucat\u0103 de o mare poft\u0103; \u0219i chiar fiii lui Israel au \u00eenceput s\u0103 pl\u00e2ng\u0103 din nou \u0219i s\u0103 zic\u0103: \u201eCine ne va da carne s\u0103 m\u00e2nc\u0103m?\u201d",
+    "\u201eNe aducem aminte de pe\u015ftele pe care-l m\u00e2ncam \u00een Egipt pe degeaba, de castraveciori, de pepeni, de praz, de ceap\u0103 \u0219i de usturoi.",
+    "Dar acum sufletul nostru este uscat; nu este nimic: ochii no\u015ftri nu v\u0103d dec\u00e2t mana aceasta!\u201d",
+    "Mana era ca bobul de coriandru \u0219i ar\u0103ta ca r\u0103\u015fina de bdeliu.",
+    "Poporul se \u00eempr\u0103\u015ftia, o aduna, o m\u0103cina la moar\u0103 sau o zdrobea \u00een piu\u0103, o fierbea \u00een oal\u0103 \u0219i f\u0103cea turte din ea; gustul ei era ca gustul unei turte preparate cu ulei.",
+    "C\u00e2nd c\u0103dea roua noaptea peste tab\u0103r\u0103, c\u0103dea \u0219i mana peste ea.",
+    "Moise a auzit poporul pl\u00e2ng\u00e2nd, \u00een toate familiile lor, fiecare la intrarea cortului s\u0103u. M\u00e2nia DOMNULUI S-a aprins foarte tare, iar Moise a fost m\u00e2hnit.",
+    "Moise I-a zis DOMNULUI: \u201eDe ce l-ai f\u0103cut pe robul T\u0103u s\u0103 sufere \u0219i de ce nu am g\u0103sit \u00eendurare \u00een ochii T\u0103i, de ai pus peste mine sarcina acestui popor \u00eentreg?\u201d",
+    "\u201eOare eu am z\u0103mislit tot poporul acesta? Oare eu l-am n\u0103scut, ca s\u0103-mi zici: \u00abPoart\u0103-l la s\u00e2nul t\u0103u cum poart\u0103 doica pe copilul care suge\u00bb, p\u00e2n\u0103 \u00een \u021bara pe care ai jurat c\u0103 o vei da p\u0103rin\u021bilor lor?\u201d",
+    "\u201eDe unde s\u0103 am eu carne ca s\u0103 dau la tot poporul acesta? C\u0103ci ei pl\u00e2ng l\u00e2ng\u0103 mine, zic\u00e2nd: \u00abD\u0103-ne carne s\u0103 m\u00e2nc\u0103m!\u00bb\u201d",
+    "\u201eEu singur nu pot s\u0103 port tot poporul acesta, c\u0103ci este prea greu pentru mine.",
+    "Dac\u0103 Te por\u021bi a\u015fa cu mine, ucide-m\u0103 mai bine, te rog, dac\u0103 am g\u0103sit \u00eendurare \u00een ochii T\u0103i, ca s\u0103 nu-mi mai v\u0103d nenorocirea!\u201d",
+    "DOMNUL i-a spus lui Moise: \u201eStr\u00e2nge-Mi \u0219aptezeci de b\u0103rba\u021bi dintre b\u0103tr\u00e2nii lui Israel, pe care-i cunoa\u015fti ca b\u0103tr\u00e2ni ai poporului \u0219i supraveghetori ai lui; adu-i la Cortul \u00cent\u00e2lnirii \u0219i s\u0103 se \u00eenf\u0103\u021bi\u015feze acolo cu tine.",
+    "Eu M\u0103 voi cobor\u00ee \u0219i voi vorbi acolo cu tine; voi lua din Duhul care este peste tine \u0219i-L voi pune peste ei, ca s\u0103 poarte \u00eempreun\u0103 cu tine sarcina poporului \u0219i s\u0103 nu o mai por\u021bi tu singur.",
+    "\u015ei spune poporului: \u00abSfin\u021bi\u021bi-v\u0103 pentru m\u00e2ine \u0219i ve\u021bi m\u00e2nca carne, pentru c\u0103 a\u021bi pl\u00e2ns la urechile DOMNULUI, zic\u00e2nd: \u201eCine ne va da carne s\u0103 m\u00e2nc\u0103m? C\u0103ci ne era bine \u00een Egipt!\u201d De aceea DOMNUL v\u0103 va da carne \u0219i ve\u021bi m\u00e2nca.",
+    "Ve\u021bi m\u00e2nca nu doar o zi, nici dou\u0103 zile, nici cinci zile, nici zece zile, nici dou\u0103zeci de zile,",
+    "ci o lun\u0103 \u00eentreag\u0103, p\u00e2n\u0103 v\u0103 va ie\u015fi pe n\u0103ri \u0219i v\u0103 va fi sc\u00e2rb\u0103 de ea; pentru c\u0103 L-a\u021bi dispretu it pe DOMNUL care este \u00een mijlocul vostru \u0219i a\u021bi pl\u00e2ns \u00eenaintea Lui, zic\u00e2nd: \u201eDe ce am ie\u015fit noi din Egipt?\u201d\u00bb\u201d",
+    "Moise a zis: \u201e\u015ease sute de mii de b\u0103rba\u021bi pedestri are poporul \u00een mijlocul c\u0103ruia m\u0103 aflu, \u0219i Tu zici: \u00abLe voi da carne \u0219i vor m\u00e2nca o lun\u0103 \u00eentreag\u0103!\u00bb\u201d",
+    "\u201eOare li se vor t\u0103ia turme \u0219i cirezi ca s\u0103 le ajung\u0103? Sau se vor str\u00e2nge to\u021bi pe\u015ftii m\u0103rii ca s\u0103 le ajung\u0103?\u201d",
+    "DOMNUL i-a r\u0103spuns lui Moise: \u201eOare m\u00e2na DOMNULUI este scurtat\u0103? Vei vedea acum dac\u0103 ce \u021bi-am spus se va \u00eemplini sau nu.\u201d",
+    "Moise a ie\u015fit \u0219i a spus poporului cuv\u00e2ntele DOMNULUI. A str\u00e2ns \u0219aptezeci de b\u0103rba\u021bi dintre b\u0103tr\u00e2nii poporului \u0219i i-a pus s\u0103 stea \u00een jurul Cortului.",
+    "DOMNUL S-a cobor\u00e2t \u00een nor \u0219i i-a vorbit; a luat din Duhul care era peste el \u0219i L-a pus peste cei \u0219aptezeci de b\u0103rba\u021bi b\u0103tr\u00e2ni. \u015ei c\u00e2nd Duhul S-a odihnit peste ei, au prorocit, dar nu au mai continuat.",
+    "Doi b\u0103rba\u021bi r\u0103m\u0103seser\u0103 \u00een tab\u0103r\u0103: numele unuia era Eldad, iar numele celuilalt Medad. Duhul S-a odihnit peste ei \u2014 c\u0103ci erau dintre cei \u00eenscri\u015fi, m\u0103car c\u0103 nu ie\u015fiser\u0103 la Cort \u2014 \u0219i au prorocit \u00een tab\u0103r\u0103.",
+    "Un t\u00e2n\u0103r a alergat \u0219i i-a spus lui Moise: \u201eEldad \u0219i Medad prorocesc \u00een tab\u0103r\u0103!\u201d",
+    "Iosua, fiul lui Nun, slujitorul lui Moise din tinere\u021bea lui, a luat cuv\u00e2ntul \u0219i a zis: \u201eSt\u0103p\u00e2ne Moise, opre\u015fte-i!\u201d",
+    "Dar Moise i-a r\u0103spuns: \u201eE\u015fti gelos pentru mine? O, de ar fi tot poporul DOMNULUI alc\u0103tuit din proroci \u0219i de ar pune DOMNUL Duhul Lui peste ei!\u201d",
+    "Apoi Moise s-a \u00eentors \u00een tab\u0103r\u0103, el \u0219i b\u0103tr\u00e2nii lui Israel.",
+    "Un v\u00e2nt st\u00e2rnit de DOMNUL a adus prepeli\u021be de peste mare \u0219i le-a l\u0103sat s\u0103 cad\u0103 peste tab\u0103r\u0103, cale de o zi \u00eentr-o parte \u0219i cale de o zi \u00een cealalt\u0103 parte \u00een jurul taberei, cam la doi co\u021bi deasupra fe\u021bei p\u0103m\u00e2ntului.",
+    "Poporul s-a sculat \u00een toat\u0103 ziua aceea, toat\u0103 noaptea \u0219i toat\u0103 ziua urm\u0103toare \u0219i a str\u00e2ns prepeli\u021bele; cel ce str\u00e2nsese cel mai pu\u021bin str\u00e2nsese zece omeri. \u015ei le-au \u00eentins de jur \u00eemprejurul taberei.",
+    "Carnea era \u00eenc\u0103 \u00een din\u021bii lor, f\u0103r\u0103 s\u0103 fie mestecata, c\u00e2nd m\u00e2nia DOMNULUI S-a aprins \u00eempotriva poporului \u0219i DOMNUL a lovit poporul cu o urgie foarte mare.",
+    "Locului aceluia i-au pus numele Chibrot-Hataava, pentru c\u0103 acolo au \u00eengropat poporul apucat de poft\u0103.",
+    "De la Chibrot-Hataava poporul a pornit spre Hate\u021bot \u0219i au r\u0103mas la Hate\u021bot.",
+  ],
+}
