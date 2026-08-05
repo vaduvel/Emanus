c86789a09b79068bf7dedc23204312b7b2881b14
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 09:23:27 +0300

    Exod 18: textul biblic din Biblia Emanus

--- packages/shared/src/bible/exodText18.ts
+++ packages/shared/src/bible/exodText18.ts
@@ -1,15 +1,39 @@
 /*
- * Textul biblic al cărții Exod, capitolul 18.
+ * Textul biblic al c\u0103r\u021bii Exod, capitolul 18.
  *
- * ATENȚIE, agentul care lucrează la Biblia Emanus:
- * fișierul acesta ține LOCUL textului, nu textul. Fiecare intrare din tablou
- * este un verset, în ordine, de la 1 la 27. Înlocuiește fiecare șir cu versetul
- * din Biblia Emanus (validată din trei părți) și nu schimba nimic altceva:
- * nici numărul intrărilor, nici cheia capitolului, nici numele exportului.
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/EXO.18.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 27 de versete.
  */
 
-const PLACEHOLDER = "[textul biblic se completează din Biblia Emanus]"
-
 export const EXOD_TEXT_18: Record<number, readonly string[]> = {
-  18: Array.from({ length: 27 }, (_, i) => `[Exod 18:${i + 1}] ${PLACEHOLDER}`),
+  18: [
+    "Ietro, preotul Madianului \u0219i socrul lui Moise, a auzit tot ce f\u0103cuse Dumnezeu pentru Moise \u0219i pentru Israel, poporul S\u0103u, \u0219i cum \u00eel scosese DOMNUL pe Israel din Egipt.",
+    "Ietro, socrul lui Moise, a luat-o pe Sefora, so\u021bia lui Moise, dup\u0103 ce acesta o trimisese \u00eenapoi,",
+    "\u00eempreun\u0103 cu cei doi fii ai ei. Unul se numea Gher\u0219om, c\u0103ci Moise spusese: \u201eAm locuit ca str\u0103in \u00eentr-o \u021bar\u0103 str\u0103in\u0103.\u201d",
+    "Cel\u0103lalt se numea Eliezer, c\u0103ci Moise spusese: \u201eDumnezeul tat\u0103lui meu mi-a fost ajutor \u0219i m-a izb\u0103vit de sabia faraonului.\u201d",
+    "Ietro, socrul lui Moise, a venit la Moise \u00een pustie, \u00eempreun\u0103 cu fiii \u0219i so\u021bia lui Moise, acolo unde acesta \u00ee\u0219i a\u0219ezase tab\u0103ra, la muntele lui Dumnezeu.",
+    "I-a transmis lui Moise: \u201eEu, socrul t\u0103u Ietro, vin la tine \u00eempreun\u0103 cu so\u021bia ta \u0219i cu cei doi fii ai ei.\u201d",
+    "Moise a ie\u0219it \u00een \u00eent\u00e2mpinarea socrului s\u0103u, s-a plecat \u0219i l-a s\u0103rutat. S-au \u00eentrebat unul pe altul de s\u0103n\u0103tate, apoi au intrat \u00een cort.",
+    "Moise i-a povestit socrului s\u0103u tot ce f\u0103cuse DOMNUL faraonului \u0219i egiptenilor pentru Israel, toate greut\u0103\u021bile care \u00eei \u00eent\u00e2mpinaser\u0103 pe drum \u0219i cum \u00eei izb\u0103vise DOMNUL.",
+    "Ietro s-a bucurat de tot binele pe care DOMNUL \u00eel f\u0103cuse lui Israel, izb\u0103vindu-l din m\u00e2na egiptenilor.",
+    "Ietro a spus: \u201eBinecuv\u00e2ntat s\u0103 fie DOMNUL, Care v-a izb\u0103vit din m\u00e2na egiptenilor \u0219i din m\u00e2na faraonului \u0219i Care a izb\u0103vit poporul de sub puterea egiptenilor!",
+    "Acum \u0219tiu c\u0103 DOMNUL este mai mare dec\u00e2t to\u021bi dumnezeii, tocmai \u00een lucrul \u00een care egiptenii s-au purtat cu trufie \u00eempotriva lor.\u201d",
+    "Ietro, socrul lui Moise, a adus pentru Dumnezeu o ardere-de-tot \u0219i alte jertfe. Aaron \u0219i to\u021bi b\u0103tr\u00e2nii lui Israel au venit s\u0103 m\u0103n\u00e2nce \u00eempreun\u0103 cu socrul lui Moise \u00eenaintea lui Dumnezeu.",
+    "A doua zi, Moise s-a a\u0219ezat s\u0103 judece poporul, iar oamenii au stat \u00een jurul lui de diminea\u021ba p\u00e2n\u0103 seara.",
+    "C\u00e2nd socrul lui Moise a v\u0103zut tot ce f\u0103cea acesta pentru popor, a spus: \u201eCe faci tu pentru popor? De ce stai singur, iar tot poporul st\u0103 \u00een jurul t\u0103u de diminea\u021ba p\u00e2n\u0103 seara?\u201d",
+    "Moise i-a r\u0103spuns socrului s\u0103u: \u201ePentru c\u0103 poporul vine la mine s\u0103-L \u00eentrebe pe Dumnezeu.",
+    "C\u00e2nd au o ne\u00een\u021belegere, vin la mine; eu judec \u00eentre un om \u0219i semenul s\u0103u \u0219i le fac cunoscute hot\u0103r\u00e2rile \u0219i legile lui Dumnezeu.\u201d",
+    "Socrul lui Moise i-a spus: \u201eCeea ce faci nu este bine.",
+    "Te vei epuiza cu siguran\u021b\u0103, at\u00e2t tu, c\u00e2t \u0219i poporul acesta care este cu tine, c\u0103ci sarcina este prea grea pentru tine. Nu o po\u021bi \u00eendeplini singur.",
+    "Ascult\u0103 acum glasul meu. \u00ce\u021bi voi da un sfat, iar Dumnezeu s\u0103 fie cu tine. Tu s\u0103 reprezin\u021bi poporul \u00eenaintea lui Dumnezeu \u0219i s\u0103 aduci ne\u00een\u021belegerile lor \u00eenaintea lui Dumnezeu.",
+    "S\u0103-i \u00eenve\u021bi hot\u0103r\u00e2rile \u0219i legile, s\u0103 le ar\u0103\u021bi calea pe care trebuie s\u0103 umble \u0219i lucrarea pe care trebuie s\u0103 o fac\u0103.",
+    "Alege din tot poporul b\u0103rba\u021bi capabili, care se tem de Dumnezeu, oameni de \u00eencredere, care ur\u0103sc c\u00e2\u0219tigul nedrept. Pune-i peste oameni drept c\u0103petenii peste mii, peste sute, peste cincizeci \u0219i peste zeci.",
+    "Ei s\u0103 judece poporul \u00een orice vreme. Orice problem\u0103 grea s\u0103 o aduc\u0103 la tine, iar problemele mici s\u0103 le judece ei. Astfel \u00ee\u021bi va fi mai u\u0219or, iar ei vor purta povara \u00eempreun\u0103 cu tine.",
+    "Dac\u0103 vei face lucrul acesta \u0219i dac\u0103 Dumnezeu \u00ee\u021bi porunce\u0219te astfel, vei putea rezista, iar tot poporul acesta va ajunge \u00een pace la locul s\u0103u.\u201d",
+    "Moise a ascultat glasul socrului s\u0103u \u0219i a f\u0103cut tot ce spusese el.",
+    "Moise a ales b\u0103rba\u021bi capabili din tot Israelul \u0219i i-a pus conduc\u0103tori peste popor: c\u0103petenii peste mii, peste sute, peste cincizeci \u0219i peste zeci.",
+    "Ei judecau poporul \u00een orice vreme. Problemele grele le aduceau la Moise, iar problemele mici le judecau ei \u00een\u0219i\u0219i.",
+    "Apoi Moise i-a dat drumul socrului s\u0103u, iar acesta a plecat \u00een \u021bara lui.",
+  ],
 }
