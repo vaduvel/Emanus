e50fa23b1ccc605639ebb72e8a963f3e37370c94
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-05 15:52:41 +0300

    Numeri 25: textul biblic din Biblia Emanus

---
+++ packages/shared/src/bible/numeriText25.ts
@@ -0,0 +1,30 @@
+/*
+ * Textul biblic al c\u0103r\u021bii Numeri, capitolul 25.
+ *
+ * Sursa: Biblia Emanus (BE), docs/data/biblia-emanus/NUM.25.json.
+ * Capitol cu status "published", auditat verset cu verset din WLC-OSHB,
+ * cu WEBU ca baz\u0103 public-domain. 18 versete.
+ */
+
+export const NUMERI_TEXT_25: Record<number, readonly string[]> = {
+  25: [
+    "Israel locuia la \u015eitim; \u0219i poporul a \u00eenceput s\u0103 se dedea la curvie cu fiicele Moabului.",
+    "Ele au poftit poporul la jertfele zeilor lor; poporul a m\u00e2ncat \u0219i s-a \u00eenchinat zeilor lor.",
+    "Israel s-a alipit de Baal-Peor; \u0219i m\u00e2nia DOMNULUI S-a aprins \u00eempotriva lui Israel.",
+    "DOMNUL i-a spus lui Moise: \u201eSp\u00e2nzur\u0103 pe toate c\u0103peteniile poporului \u00eenaintea DOMNULUI, \u00een fa\u021ba soarelui, pentru ca m\u00e2nia aprins\u0103 a DOMNULUI s\u0103 se dep\u0103rteze de la Israel.\u201d",
+    "Moise le-a spus judec\u0103torilor lui Israel: \u201eUcide\u021bi fiecare dintre oamenii vo\u015ftri pe cei ce s-au alipit de Baal-Peor!\u201d",
+    "\u015ei iat\u0103 c\u0103 un b\u0103rbat dintre fiii lui Israel a venit \u0219i a adus la fra\u021bii lui pe o madianit\u0103, sub ochii lui Moise \u0219i sub ochii \u00eentregii adun\u0103ri a fiilor lui Israel, pe c\u00e2nd pl\u00e2ngeau la intrarea Cortului \u00cent\u00e2lnirii.",
+    "C\u00e2nd a v\u0103zut Fineas, fiul lui Eleazar, fiul preotului Aaron, s-a sculat din mijlocul adun\u0103rii, a luat o suli\u021b\u0103 \u00een m\u00e2n\u0103,",
+    "s-a dus dup\u0103 b\u0103rbatul din Israel p\u00e2n\u0103 \u00een cort \u0219i i-a str\u0103puns pe am\u00e2ndoi: pe b\u0103rbatul din Israel \u0219i pe femeie prin p\u00e2ntece. \u015ei urgia s-a oprit de la fiii lui Israel.",
+    "Cei ce au murit de urgie au fost dou\u0103zeci \u0219i patru de mii.",
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eFineas, fiul lui Eleazar, fiul preotului Aaron, a ab\u0103tut m\u00e2nia Mea de la fiii lui Israel, plin fiind de r\u00e2vna Mea \u00een mijlocul lor; de aceea nu i-am mistuit pe fiii lui Israel \u00een r\u00e2vna Mea.",
+    "De aceea spune-i c\u0103 \u00ce\u00ee dau leg\u0103m\u00e2ntul Meu de pace;",
+    "acesta va fi pentru el \u0219i pentru urma\u015fii lui leg\u0103m\u00e2ntul unei preo\u021bii ve\u015fnice, pentru c\u0103 a fost plin de r\u00e2vn\u0103 pentru Dumnezeul lui \u0219i a f\u0103cut isp\u0103\u015fire pentru fiii lui Israel.\u201d",
+    "Numele b\u0103rbatului din Israel ucis \u00eempreun\u0103 cu madianita era Zimri, fiul lui Salu, c\u0103petenia unei case p\u0103rinte\u015fti a simiioni\u021bilor.",
+    "Iar numele femeii madianite ucise era Cozbi, fiica lui \u0162ur, cap al unor neamuri dintr-o cas\u0103 p\u0103rinteasc\u0103 din Madian.",
+    "DOMNUL i-a vorbit lui Moise, spun\u00e2nd:",
+    "\u201eTrata\u021bi-i pe madiani\u021bi ca pe ni\u015fte vr\u0103jma\u015fi \u0219i lovi\u021bi-i;",
+    "c\u0103ci v-au fost vr\u0103jma\u015fi prin vicleniile lor cu care v-au am\u0103git \u00een lucrul lui Peor \u0219i \u00een lucrul Cozbiei, fiica unei c\u0103petenii din Madian, sora lor, care a fost ucis\u0103 \u00een ziua urgiei pricinuite de Peor.\u201d",
+  ],
+}
