af52f7623514396852d4402489af0b133189637a
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-08 19:14:04 +0300

    wire reviewed Ezekiel 36, 43, 47 and 48 explanations

--- packages/shared/src/bible/overlays/ezechielEditorialOverlay.ts
+++ packages/shared/src/bible/overlays/ezechielEditorialOverlay.ts
@@ -2,6 +2,7 @@ import { assertCompleteOverlay, type ExplainedBookOverlay } from "../explainedOv
 import { VT_MAJOR_PROPHETS_FULL } from "../vtFullNarrativesMajorProphets.js"
 import { reviewEzechiel14And18Explanations } from "./ezechiel14And18ExplanationReview.js"
 import { reviewEzechiel16And28Explanations } from "./ezechiel16And28ExplanationReview.js"
+import { reviewEzechiel36_43_47_48Explanations } from "./ezechiel36_43_47_48ExplanationReview.js"
 import { EZECHIEL_EXPLAINED as POONEN_BASE } from "./ezechielOverlay.js"
 import { textualChapter } from "./textualChapterFactory.js"
 
@@ -16,8 +17,10 @@ const EZECHIEL_EDITORIAL_BASE: ExplainedBookOverlay = {
   chapters,
 }
 
-const EZECHIEL_EDITORIAL_REVIEWED = reviewEzechiel16And28Explanations(
-  reviewEzechiel14And18Explanations(EZECHIEL_EDITORIAL_BASE),
+const EZECHIEL_EDITORIAL_REVIEWED = reviewEzechiel36_43_47_48Explanations(
+  reviewEzechiel16And28Explanations(
+    reviewEzechiel14And18Explanations(EZECHIEL_EDITORIAL_BASE),
+  ),
 )
 
 export const EZECHIEL_EDITORIAL_EXPLAINED = assertCompleteOverlay(EZECHIEL_EDITORIAL_REVIEWED, 48)
