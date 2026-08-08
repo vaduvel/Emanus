02977d0ab781a46b3c58383aaf4c1a1494346d91
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-08 06:20:42 +0300

    feat(bible): materialize reviewed Osea candidate

---
+++ packages/shared/src/bible/generated/vtCanonicalText/oseaReviewedText.ts
@@ -0,0 +1,191 @@
+import { OSEA_TEXT } from "./oseaText.js"
+
+export const OSEA_REVIEWED_CORRECTIONS =
+  // OSEA_REVIEWED_CORRECTIONS_JSON_START
+{
+  "1": {
+    "1": "Cuvântul DOMNULUI care a venit la Osea, fiul lui Beeri, pe vremea lui Ozia, Iotam, Ahaz și Ezechia, împărații lui Iuda, și pe vremea lui Ieroboam, fiul lui Ioas, împăratul lui Israel.",
+    "2": "Când DOMNUL a început să vorbească prin Osea, DOMNUL i-a spus lui Osea: „Du-te și ia-ți o soție desfrânată și copii ai desfrânării, căci țara se desfrânează mult, părăsindu-L pe DOMNUL.”",
+    "3": "El s-a dus și a luat-o pe Gomer, fiica lui Diblaim. Ea a zămislit și i-a născut un fiu.",
+    "4": "DOMNUL i-a zis: „Pune-i numele Izreel, căci peste puțină vreme voi cere socoteală casei lui Iehu pentru sângele vărsat la Izreel și voi pune capăt domniei casei lui Israel.”",
+    "9": "El a zis: „Pune-i numele Lo-Ami, căci voi nu sunteți poporul Meu, iar Eu nu voi fi al vostru.”",
+    "10": "Totuși, numărul copiilor lui Israel va fi ca nisipul mării, care nu se poate nici măsura, nici număra. Și, în locul unde li se zicea: „Nu sunteți poporul Meu”, li se va spune: „Fiii Dumnezeului celui viu.”"
+  },
+  "2": {
+    "2": "Certați-vă cu mama voastră, certați-vă, căci ea nu este soția Mea și Eu nu sunt soțul ei! Să-și îndepărteze desfrânările de pe fața ei și adulterele dintre sânii ei!",
+    "3": "Altfel, o voi dezbrăca și o voi lăsa goală ca în ziua nașterii ei; o voi face ca un pustiu, ca un pământ uscat, și o voi lăsa să moară de sete.",
+    "6": "De aceea, iată, îți voi îngrădi calea cu spini și voi ridica un zid împotriva ei, ca să nu-și mai găsească potecile.",
+    "7": "Va alerga după iubiții ei, dar nu-i va ajunge; îi va căuta, dar nu-i va găsi. Atunci va spune: „Mă voi întoarce la primul meu soț, căci îmi era mai bine atunci decât acum.”",
+    "8": "Ea n-a știut că Eu îi dădeam grâul, mustul și untdelemnul și că Eu îi înmulțeam argintul și aurul, pe care ei le-au folosit pentru Baal.",
+    "9": "De aceea Îmi voi lua înapoi grâul la vremea lui și mustul la vremea lui și Îmi voi lua lâna și inul menite să-i acopere goliciunea.",
+    "12": "Îi voi pustii viile și smochinii, despre care spunea: „Aceasta este plata pe care mi-au dat-o iubiții mei!” Le voi face o pădure, iar animalele câmpului le vor mânca.",
+    "13": "O voi pedepsi pentru zilele Baalilor, când le ardea tămâie, când se împodobea cu veriga de nas și cu podoabele ei și mergea după iubiții ei, iar pe Mine Mă uita, zice DOMNUL.",
+    "14": "De aceea, iată, o voi ademeni, o voi duce în pustiu și îi voi vorbi inimii.",
+    "15": "De acolo îi voi da viile și voi face din valea Acor o ușă a speranței. Ea va răspunde acolo ca în zilele tinereții ei și ca în ziua când a ieșit din țara Egiptului.",
+    "18": "În ziua aceea voi încheia pentru ei un legământ cu animalele câmpului, cu păsările cerului și cu târâtoarele pământului. Voi sfărâma din țară arcul, sabia și războiul și îi voi face să locuiască în siguranță.",
+    "19": "Te voi logodi cu Mine pentru totdeauna; te voi logodi cu Mine în dreptate, în judecată, în bunătate statornică și în îndurare.",
+    "20": "Te voi logodi cu Mine în credincioșie și Îl vei cunoaște pe DOMNUL.",
+    "21": "În ziua aceea voi răspunde, zice DOMNUL. Voi răspunde cerurilor, iar ele vor răspunde pământului.",
+    "22": "Pământul va răspunde grâului, mustului și untdelemnului, iar acestea vor răspunde lui Izreel.",
+    "23": "O voi semăna pentru Mine în țară și voi avea milă de Lo-Ruhama. Îi voi spune lui Lo-Ami: „Tu ești poporul Meu!”, iar el va spune: „Dumnezeul meu!”"
+  },
+  "3": {
+    "1": "DOMNUL mi-a zis: „Du-te din nou și iubește o femeie iubită de altul și adulteră, așa cum DOMNUL îi iubește pe copiii lui Israel, deși ei se întorc spre alți dumnezei și iubesc turtele de stafide.”",
+    "2": "Mi-am cumpărat-o cu cincisprezece sicli de argint, un homer de orz și un letec de orz.",
+    "4": "Căci copiii lui Israel vor rămâne multe zile fără împărat, fără căpetenie, fără jertfă, fără stâlp sacru, fără efod și fără terafimi.",
+    "5": "După aceea, copiii lui Israel se vor întoarce și Îl vor căuta pe DOMNUL Dumnezeul lor și pe David, împăratul lor; vor veni tremurând la DOMNUL și la bunătatea Lui în zilele de pe urmă."
+  },
+  "4": {
+    "1": "Ascultați cuvântul DOMNULUI, copii ai lui Israel! DOMNUL are o acuzație împotriva locuitorilor țării, căci nu este adevăr, nu este bunătate statornică și nu este cunoaștere de Dumnezeu în țară.",
+    "2": "Blestemă, mint, ucid, fură și comit adulter; trec orice hotar, iar vărsarea de sânge urmează vărsării de sânge.",
+    "3": "De aceea țara se va jeli și toți cei care locuiesc în ea se vor veșteji, împreună cu animalele câmpului și păsările cerului; chiar și peștii mării vor pieri.",
+    "10": "Vor mânca, dar nu se vor sătura; se vor desfrâna, dar nu se vor înmulți, pentru că au părăsit ascultarea de DOMNUL.",
+    "14": "Nu le voi pedepsi pe fiicele voastre când se desfrânează, nici pe nurorile voastre când comit adulter, căci bărbații înșiși se duc cu prostituatele și aduc jertfe împreună cu prostituatele de la sanctuare. Astfel, poporul fără pricepere va ajunge la ruină.",
+    "16": "Israel s-a purtat cu încăpățânare ca o vițea răzvrătită. Cum să-i mai pască acum DOMNUL ca pe un miel într-un loc larg?",
+    "18": "Băutura lor s-a acrit; ei se dedau fără încetare desfrânării. Cârmuitorii ei iubesc cu ardoare rușinea.",
+    "19": "Vântul a înfășurat-o în aripile lui, iar ei vor fi rușinați din cauza jertfelor lor."
+  },
+  "5": {
+    "2": "Răzvrătiții s-au afundat în măcel, dar Eu îi voi disciplina pe toți.",
+    "3": "Îl cunosc pe Efraim, iar Israel nu-Mi este ascuns; căci acum, Efraime, te-ai desfrânat, iar Israel s-a spurcat.",
+    "7": "Au fost necredincioși DOMNULUI, căci au născut copii nelegitimi; acum luna nouă îi va înghiți împreună cu ogoarele lor.",
+    "8": "Sunați din corn la Ghibeea și din trâmbiță la Rama! Dați strigătul de luptă la Bet-Aven! În urma ta, Beniamine!",
+    "9": "Efraim va ajunge pustiu în ziua mustrării. Între semințiile lui Israel am făcut cunoscut ceea ce este sigur.",
+    "11": "Efraim este asuprit, zdrobit de judecată, căci a ales să meargă după poruncă.",
+    "12": "Eu voi fi ca o molie pentru Efraim și ca putreziciunea pentru casa lui Iuda.",
+    "15": "Voi pleca și Mă voi întoarce în locul Meu până când își vor recunoaște vina și vor căuta fața Mea. În necazul lor Mă vor căuta cu stăruință."
+  },
+  "6": {
+    "2": "După două zile ne va înviora; în a treia zi ne va ridica și vom trăi înaintea Lui.",
+    "4": "Ce să-ți fac, Efraime? Ce să-ți fac, Iudo? Iubirea voastră statornică este ca norul de dimineață și ca roua care dispare devreme.",
+    "5": "De aceea i-am cioplit prin proroci, i-am ucis prin cuvintele gurii Mele, iar judecățile tale ies ca lumina.",
+    "7": "Dar ei, ca Adam, au călcat legământul; acolo Mi-au fost necredincioși.",
+    "10": "În casa lui Israel am văzut un lucru îngrozitor: acolo este desfrânare în Efraim, iar Israel s-a spurcat.",
+    "11": "Și pentru tine, Iudo, este pregătit un seceriș, când voi restaura soarta poporului Meu."
+  },
+  "7": {
+    "6": "Și-au pregătit inima ca un cuptor în timp ce stau la pândă; brutarul lor doarme toată noaptea, iar dimineața cuptorul arde ca un foc în flăcări.",
+    "9": "Străinii îi mănâncă puterea, iar el nu-și dă seama; fire albe i s-au răspândit prin păr, iar el nu-și dă seama.",
+    "10": "Mândria lui Israel mărturisește împotriva lui, dar, cu toate acestea, ei nu se întorc la DOMNUL Dumnezeul lor și nu-L caută.",
+    "11": "Efraim a ajuns ca o porumbiță naivă, fără pricepere: cheamă Egiptul și se duc în Asiria.",
+    "12": "Când se duc, Îmi voi întinde plasa peste ei; îi voi coborî ca pe păsările cerului și îi voi disciplina potrivit cu ceea ce a auzit adunarea lor.",
+    "14": "Nu strigă către Mine din inimă, ci se bocesc pe paturile lor. Se adună pentru grâu și must, dar se întorc de la Mine.",
+    "15": "Eu i-am instruit și le-am întărit brațele, dar ei plănuiesc răul împotriva Mea.",
+    "16": "Se întorc, dar nu la Cel Preaînalt; sunt ca un arc înșelător. Căpeteniile lor vor cădea de sabie din pricina furiei limbii lor. Aceasta va fi batjocura lor în țara Egiptului."
+  },
+  "8": {
+    "1": "Pune trâmbița la buze! Ca un vultur este peste Casa DOMNULUI, pentru că au călcat legământul Meu și s-au răzvrătit împotriva Legii Mele.",
+    "3": "Israel a lepădat binele; vrăjmașul îl va urmări.",
+    "5": "Vițelul tău este lepădat, Samario! Mânia Mea s-a aprins împotriva lor. Până când vor rămâne incapabili de curăție?",
+    "9": "Căci s-au dus în Asiria ca un măgar sălbatic care umblă singuratic. Efraim și-a tocmit iubiți.",
+    "10": "Chiar dacă plătesc printre neamuri, acum îi voi strânge; vor începe să se împuțineze sub povara împăratului și a căpeteniilor.",
+    "12": "I-am scris multele lucruri ale Legii Mele, dar ele au fost socotite ca ceva străin.",
+    "14": "Căci Israel L-a uitat pe Cel care l-a făcut și și-a zidit palate, iar Iuda a înmulțit cetățile întărite; dar voi trimite foc în cetățile lui și el îi va mistui fortărețele."
+  },
+  "9": {
+    "1": "Nu te bucura, Israele, nu tresălta de bucurie ca popoarele, căci ai fost necredincios Dumnezeului tău; ai iubit plata unei prostituate pe toate ariile de grâu.",
+    "2": "Aria și teascul nu-i vor hrăni, iar mustul o va dezamăgi.",
+    "4": "Nu vor turna DOMNULUI daruri de vin și jertfele lor nu-I vor fi plăcute. Jertfele lor vor fi pentru ei ca pâinea celor în doliu: toți cei care vor mânca din ea se vor spurca, fiindcă pâinea lor va fi numai pentru foamea lor și nu va intra în Casa DOMNULUI.",
+    "7": "Au venit zilele cercetării, au venit zilele răsplătirii. Israel îl va socoti pe proroc nebun și pe omul duhului ieșit din minți, din pricina mulțimii nelegiuirii tale și a marii tale vrăjmășii.",
+    "8": "Prorocul este străjerul lui Efraim împreună cu Dumnezeul meu; o cursă de păsărar este pe toate căile lui și vrăjmășie în casa Dumnezeului său.",
+    "11": "Slava lui Efraim va zbura ca o pasăre: nu va mai fi naștere, nici sarcină, nici zămislire.",
+    "12": "Chiar dacă își vor crește copiii, îi voi lipsi de ei până nu va mai rămâne niciun om. Vai de ei și când Mă voi depărta de ei!",
+    "13": "L-am văzut pe Efraim ca pe Tirul, sădit într-un loc plăcut; dar Efraim își va scoate copiii la ucigaș.",
+    "14": "Dă-le, DOAMNE! Ce să le dai? Dă-le un pântece care pierde sarcina și sâni uscați.",
+    "15": "Toată răutatea lor este la Ghilgal; acolo i-am urât. Din pricina răutății faptelor lor îi voi izgoni din Casa Mea. Nu-i voi mai iubi; toate căpeteniile lor sunt răzvrătite.",
+    "16": "Efraim este lovit, rădăcina lui s-a uscat și nu va mai da rod. Chiar dacă vor naște, îi voi omorî pe cei iubiți ai pântecelui lor."
+  },
+  "10": {
+    "2": "Inima lor este împărțită; acum vor fi găsiți vinovați. El le va surpa altarele și le va nimici stâlpii sacri.",
+    "3": "Acum vor spune: „Nu avem împărat, căci nu ne-am temut de DOMNUL; iar împăratul, ce ar putea face pentru noi?”",
+    "4": "Rostesc vorbe, jurăminte mincinoase și încheie legăminte; de aceea judecata încolțește ca buruiana otrăvitoare în brazdele câmpului.",
+    "5": "Locuitorii Samariei se vor teme pentru vițeii din Bet-Aven. Poporul lui va jeli pentru el, iar preoții lui, care se bucurau de el, vor plânge după slava lui, fiindcă s-a depărtat de el.",
+    "7": "Samaria și împăratul ei vor pluti departe ca o surcea pe fața apelor.",
+    "8": "Înălțimile din Aven, păcatul lui Israel, vor fi nimicite; spini și mărăcini vor crește pe altarele lor. Vor spune munților: „Acoperiți-ne!” și dealurilor: „Cădeți peste noi!”",
+    "10": "Când voi dori, îi voi disciplina; popoarele se vor aduna împotriva lor când vor fi legați pentru cele două fărădelegi ale lor.",
+    "11": "Efraim este o vițea dresată, căreia îi place să treiere. Voi pune jugul pe gâtul ei frumos; voi pune un călăreț pe Efraim. Iuda va ara, iar Iacov îi va sfărâma bulgării.",
+    "12": "Semănați pentru voi în dreptate și secerați după bunătatea statornică. Desțeleniți-vă ogorul nelucrat, căci este vremea să-L căutați pe DOMNUL până va veni și va ploua dreptate peste voi.",
+    "13": "Ați arat răutatea, ați secerat nelegiuirea și ați mâncat rodul minciunii, pentru că te-ai încrezut în calea ta și în mulțimea vitejilor tăi.",
+    "14": "De aceea se va ridica un vuiet de luptă în poporul tău și toate fortărețele tale vor fi nimicite, așa cum Șalman a nimicit Bet-Arbel în ziua luptei, când mama a fost zdrobită împreună cu copiii ei."
+  },
+  "11": {
+    "2": "Îi chemau, dar ei se depărtau tot mai mult de cei care-i chemau; aduceau jertfe Baalilor și ardeau tămâie chipurilor cioplite.",
+    "6": "Sabia va cădea peste cetățile lor, le va nimici zăvoarele și le va mistui din pricina planurilor lor.",
+    "7": "Poporul Meu este hotărât să se depărteze de Mine. Deși Îl cheamă pe Cel Preaînalt, El nu-i va înălța nicidecum.",
+    "9": "Nu voi împlini iuțimea mâniei Mele și nu Mă voi întoarce să-l nimicesc pe Efraim, căci Eu sunt Dumnezeu, nu om, Sfântul în mijlocul tău. Nu voi veni cu mânie.",
+    "10": "Îl vor urma pe DOMNUL; El va răcni ca un leu. Când va răcni, copiii vor veni tremurând de la apus.",
+    "11": "Vor veni tremurând din Egipt ca o pasăre și din țara Asiriei ca o porumbiță; îi voi așeza în casele lor, zice DOMNUL.",
+    "12": "Efraim Mă înconjoară cu minciună și casa lui Israel cu înșelăciune; iar Iuda încă rătăcește cu Dumnezeu și este credincios împreună cu cei sfinți."
+  },
+  "12": {
+    "1": "Efraim se hrănește cu vânt și urmărește vântul de răsărit; toată ziua înmulțește minciuna și pustiirea. Încheie legământ cu Asiria, iar untdelemnul este dus în Egipt.",
+    "4": "S-a luptat cu îngerul și a biruit; a plâns și s-a rugat de el. L-a întâlnit la Betel și acolo a vorbit cu noi.",
+    "6": "Tu, dar, întoarce-te la Dumnezeul tău, păstrează bunătatea statornică și dreptatea și așteaptă-L mereu pe Dumnezeul tău.",
+    "7": "Un negustor are în mână o cumpănă înșelătoare; îi place să asuprească.",
+    "9": "Dar Eu sunt DOMNUL Dumnezeul tău din țara Egiptului; te voi face din nou să locuiești în corturi, ca în zilele sărbătorii.",
+    "11": "Dacă Galaadul este nelegiuit, cu adevărat ei sunt deșertăciune. La Ghilgal jertfesc boi; altarele lor sunt ca mormanele de pietre pe brazdele câmpului.",
+    "14": "Efraim a provocat amarnic mânia; de aceea sângele lui va rămâne asupra lui, iar Domnul său îi va răsplăti ocara."
+  },
+  "13": {
+    "1": "Când vorbea Efraim, era cutremur; el se înălțase în Israel. Dar când s-a făcut vinovat prin Baal, a murit.",
+    "2": "Și acum păcătuiesc tot mai mult; își fac chipuri turnate din argintul lor, idoli după priceperea lor, toți lucrarea meșterilor. Despre ei se spune: „Cei ce aduc jertfe omenești sărută vițeii!”",
+    "4": "Dar Eu sunt DOMNUL Dumnezeul tău din țara Egiptului; să nu recunoști alt Dumnezeu în afară de Mine, căci nu este alt Mântuitor în afară de Mine.",
+    "5": "Eu te-am cunoscut în pustiu, într-o țară de mare secetă.",
+    "7": "Am fost pentru ei ca un leu; ca un leopard voi pândi lângă drum.",
+    "8": "Îi voi întâmpina ca o ursoaică lipsită de pui și le voi sfâșia învelișul inimii. Acolo îi voi devora ca o leoaică; fiara câmpului îi va sfâșia.",
+    "9": "Ești nimicit, Israele, pentru că ești împotriva Mea, împotriva ajutorului tău.",
+    "10": "Unde este acum împăratul tău, ca să te scape în toate cetățile tale? Unde sunt judecătorii tăi, despre care spuneai: „Dă-mi un împărat și căpetenii”?",
+    "13": "Îl vor apuca durerile femeii care naște. Este un fiu neînțelept, căci, atunci când vine vremea, nu se înfățișează la deschiderea pântecelui.",
+    "14": "Îi voi răscumpăra din puterea Locuinței morților și îi voi izbăvi de la moarte. Moarte, unde sunt urgiile tale? Locuință a morților, unde este nimicirea ta? Îndurarea va fi ascunsă de ochii Mei.",
+    "16": "Samaria își va purta vina, pentru că s-a răzvrătit împotriva Dumnezeului ei. Vor cădea de sabie; pruncii lor vor fi zdrobiți, iar femeile lor însărcinate vor fi spintecate."
+  },
+  "14": {
+    "2": "Luați cu voi cuvinte și întoarceți-vă la DOMNUL. Spuneți-I: „Iartă toată nelegiuirea și primește ceea ce este bun; iar noi vom aduce tauri, așa cum au făgăduit buzele noastre.”",
+    "3": "Asiria nu ne va salva. Nu vom mai încăleca pe cai și nu vom mai spune lucrării mâinilor noastre: „Dumnezeii noștri!”, căci la Tine găsește milă orfanul.",
+    "4": "Le voi vindeca rătăcirea și îi voi iubi de bunăvoie, căci mânia Mea s-a întors de la ei.",
+    "7": "Cei ce locuiesc la umbra lui se vor întoarce; vor prinde viață ca grâul și vor înflori ca via, iar faima lui va fi ca vinul Libanului.",
+    "8": "Efraime, ce mai am Eu de-a face cu idolii? Eu i-am răspuns și veghez asupra lui. Eu sunt ca un chiparos verde; de la Mine se găsește rodul tău.",
+    "9": "Cine este înțelept să înțeleagă aceste lucruri? Cine este priceput să le cunoască? Căci căile DOMNULUI sunt drepte; cei drepți umblă pe ele, dar cei răzvrătiți se poticnesc pe ele."
+  }
+}
+  // OSEA_REVIEWED_CORRECTIONS_JSON_END
+  as const
+
+function applyOseaReviewedCorrections(): Readonly<Record<number, readonly string[]>> {
+  const reviewed: Record<number, readonly string[]> = {}
+
+  for (const [chapterKey, verses] of Object.entries(OSEA_TEXT)) {
+    reviewed[Number(chapterKey)] = [...verses]
+  }
+
+  const corrections = OSEA_REVIEWED_CORRECTIONS as unknown as Readonly<
+    Record<string, Readonly<Record<string, string>>>
+  >
+
+  for (const [chapterKey, versePatches] of Object.entries(corrections)) {
+    const chapter = Number(chapterKey)
+    const baseVerses = reviewed[chapter]
+    if (!baseVerses) {
+      throw new Error(`[Osea ${chapter}] lipsește capitolul de bază pentru candidatul revizuit.`)
+    }
+
+    const next = [...baseVerses]
+    for (const [verseKey, text] of Object.entries(versePatches)) {
+      const verse = Number(verseKey)
+      const index = verse - 1
+      if (index < 0 || index >= next.length) {
+        throw new Error(`[Osea ${chapter}:${verse}] corecție în afara versificației de bază.`)
+      }
+      next[index] = text
+    }
+    reviewed[chapter] = Object.freeze(next)
+  }
+
+  return Object.freeze(reviewed)
+}
+
+/**
+ * Candidatul Osea rezultat din fresh-source semantic review 1–14.
+ * Rămâne `temporary-editorial` până la gate-ul separat de promovare Biblia Emanus.
+ */
+export const OSEA_REVIEWED_TEXT = applyOseaReviewedCorrections()
