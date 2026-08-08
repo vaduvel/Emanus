0f826244df06c46b4e3228632e7cf6363844408d
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-08 19:51:11 +0300

    wire reviewed Proverbs 23 explanation

--- packages/shared/src/bible/overlays/proverbeEditorialOverlay.ts
+++ packages/shared/src/bible/overlays/proverbeEditorialOverlay.ts
@@ -1,5 +1,6 @@
 import { assertCompleteOverlay, type ExplainedBookOverlay } from "../explainedOverlay.js"
 import { reviewProverbe03_22_31Explanations } from "./proverbe03_22_31ExplanationReview.js"
+import { reviewProverbe23Explanation } from "./proverbe23ExplanationReview.js"
 import { PROVERBE_EXPLAINED as POONEN_BASE } from "./proverbeOverlay.js"
 import { PROVERBE_DEEPENED_11_20 } from "./proverbeDeepened11_20.js"
 import { PROVERBE_DEEPENED_21_30 } from "./proverbeDeepened21_30.js"
@@ -26,6 +27,8 @@ const PROVERBE_EDITORIAL_BASE: ExplainedBookOverlay = {
   chapters,
 }
 
-const PROVERBE_EDITORIAL_REVIEWED = reviewProverbe03_22_31Explanations(PROVERBE_EDITORIAL_BASE)
+const PROVERBE_EDITORIAL_REVIEWED = reviewProverbe23Explanation(
+  reviewProverbe03_22_31Explanations(PROVERBE_EDITORIAL_BASE),
+)
 
 export const PROVERBE_EDITORIAL_EXPLAINED = assertCompleteOverlay(PROVERBE_EDITORIAL_REVIEWED, 31)
