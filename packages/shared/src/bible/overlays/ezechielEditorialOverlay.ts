25f05c4735beb56b9602a34876df443be25bb778
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-08 22:58:58 +0300

    Bible review: apply Ezekiel 14 fidelity last

--- packages/shared/src/bible/overlays/ezechielEditorialOverlay.ts
+++ packages/shared/src/bible/overlays/ezechielEditorialOverlay.ts
@@ -3,6 +3,7 @@ import { VT_MAJOR_PROPHETS_FULL } from "../vtFullNarrativesMajorProphets.js"
 import { reviewEzechiel14And18Explanations } from "./ezechiel14And18ExplanationReview.js"
 import { reviewEzechiel16And28Explanations } from "./ezechiel16And28ExplanationReview.js"
 import { reviewEzechiel36_43_47_48Explanations } from "./ezechiel36_43_47_48ExplanationReview.js"
+import { restoreEzechiel14PoonenFidelity } from "./ezechiel14PoonenFidelityReview.js"
 import { restoreEzechiel36_43_47_48PoonenFidelity } from "./ezechiel36_43_47_48PoonenFidelityReview.js"
 import { restoreEzechielPoonenFidelity } from "./ezechielPoonenFidelityReview.js"
 import { EZECHIEL_EXPLAINED as POONEN_BASE } from "./ezechielOverlay.js"
@@ -19,11 +20,13 @@ const EZECHIEL_EDITORIAL_BASE: ExplainedBookOverlay = {
   chapters,
 }
 
-const EZECHIEL_EDITORIAL_REVIEWED = restoreEzechiel36_43_47_48PoonenFidelity(
-  restoreEzechielPoonenFidelity(
-    reviewEzechiel36_43_47_48Explanations(
-      reviewEzechiel16And28Explanations(
-        reviewEzechiel14And18Explanations(EZECHIEL_EDITORIAL_BASE),
+const EZECHIEL_EDITORIAL_REVIEWED = restoreEzechiel14PoonenFidelity(
+  restoreEzechiel36_43_47_48PoonenFidelity(
+    restoreEzechielPoonenFidelity(
+      reviewEzechiel36_43_47_48Explanations(
+        reviewEzechiel16And28Explanations(
+          reviewEzechiel14And18Explanations(EZECHIEL_EDITORIAL_BASE),
+        ),
       ),
     ),
   ),
