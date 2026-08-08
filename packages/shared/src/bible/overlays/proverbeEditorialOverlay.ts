6ce18c4159f5c4c5c2135729aa04cac17672fc57
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-08 22:43:01 +0300

    Bible review: apply Proverbs Poonen fidelity last

--- packages/shared/src/bible/overlays/proverbeEditorialOverlay.ts
+++ packages/shared/src/bible/overlays/proverbeEditorialOverlay.ts
@@ -4,6 +4,7 @@ import { reviewProverbe23Explanation } from "./proverbe23ExplanationReview.js"
 import { PROVERBE_EXPLAINED as POONEN_BASE } from "./proverbeOverlay.js"
 import { PROVERBE_DEEPENED_11_20 } from "./proverbeDeepened11_20.js"
 import { PROVERBE_DEEPENED_21_30 } from "./proverbeDeepened21_30.js"
+import { restoreProverbePoonenFidelity } from "./proverbePoonenFidelityReview.js"
 import { PROVERBE_TEXTUAL_CHAPTERS } from "./proverbeTextualChapters.js"
 
 const chapters = POONEN_BASE.chapters.map((chapter) => {
@@ -27,8 +28,10 @@ const PROVERBE_EDITORIAL_BASE: ExplainedBookOverlay = {
   chapters,
 }
 
-const PROVERBE_EDITORIAL_REVIEWED = reviewProverbe23Explanation(
-  reviewProverbe03_22_31Explanations(PROVERBE_EDITORIAL_BASE),
+const PROVERBE_EDITORIAL_REVIEWED = restoreProverbePoonenFidelity(
+  reviewProverbe23Explanation(
+    reviewProverbe03_22_31Explanations(PROVERBE_EDITORIAL_BASE),
+  ),
 )
 
 export const PROVERBE_EDITORIAL_EXPLAINED = assertCompleteOverlay(PROVERBE_EDITORIAL_REVIEWED, 31)
