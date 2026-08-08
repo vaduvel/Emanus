eff3f319c03fc8d1a335c426dea1436fcad34494
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-08 22:50:21 +0300

    Bible review: apply Daniel 4 and 6 fidelity last

--- packages/shared/src/bible/overlays/danielEditorialOverlay.ts
+++ packages/shared/src/bible/overlays/danielEditorialOverlay.ts
@@ -2,6 +2,7 @@ import { assertCompleteOverlay, type ExplainedBookOverlay } from "../explainedOv
 import { VT_MAJOR_PROPHETS_FULL } from "../vtFullNarrativesMajorProphets.js"
 import { reviewDaniel03_07_09_10_12Explanations } from "./daniel03_07_09_10_12ExplanationReview.js"
 import { reviewDaniel04And06Explanations } from "./daniel04And06ExplanationReview.js"
+import { restoreDaniel04And06PoonenFidelity } from "./daniel04And06PoonenFidelityReview.js"
 import { restoreDanielPoonenFidelity } from "./danielPoonenFidelityReview.js"
 import { DANIEL_EXPLAINED as POONEN_BASE } from "./danielOverlay.js"
 import { textualChapter } from "./textualChapterFactory.js"
@@ -17,9 +18,11 @@ const DANIEL_EDITORIAL_BASE: ExplainedBookOverlay = {
   chapters,
 }
 
-const DANIEL_EDITORIAL_REVIEWED = restoreDanielPoonenFidelity(
-  reviewDaniel04And06Explanations(
-    reviewDaniel03_07_09_10_12Explanations(DANIEL_EDITORIAL_BASE),
+const DANIEL_EDITORIAL_REVIEWED = restoreDaniel04And06PoonenFidelity(
+  restoreDanielPoonenFidelity(
+    reviewDaniel04And06Explanations(
+      reviewDaniel03_07_09_10_12Explanations(DANIEL_EDITORIAL_BASE),
+    ),
   ),
 )
 
