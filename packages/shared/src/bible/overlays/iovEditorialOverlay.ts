a699824c49a5501396a3c1beb8a7fd0b71951772
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-09 08:58:51 +0300

    Wire final Job 29 Poonen fidelity layer

--- packages/shared/src/bible/overlays/iovEditorialOverlay.ts
+++ packages/shared/src/bible/overlays/iovEditorialOverlay.ts
@@ -1,6 +1,7 @@
 import { assertCompleteOverlay, type ExplainedBookOverlay } from "../explainedOverlay.js"
 import { VT_WISDOM_FULL } from "../vtFullNarrativesWisdom.js"
 import { reviewIov29_31_38_40_42Explanations } from "./iov29_31_38_40_42ExplanationReview.js"
+import { restoreIov29FinalPoonenFidelity } from "./iov29FinalPoonenFidelityReview.js"
 import { IOV_EXPLAINED as POONEN_BASE } from "./iovOverlay.js"
 import { restoreIovPoonenFidelity } from "./iovPoonenFidelityReview.js"
 import { IOV_TEXTUAL_CHAPTERS } from "./iovTextualChapters.js"
@@ -18,8 +19,10 @@ const IOV_EDITORIAL_BASE: ExplainedBookOverlay = {
   chapters,
 }
 
-const IOV_EDITORIAL_REVIEWED = restoreIovPoonenFidelity(
-  reviewIov29_31_38_40_42Explanations(IOV_EDITORIAL_BASE),
+const IOV_EDITORIAL_REVIEWED = restoreIov29FinalPoonenFidelity(
+  restoreIovPoonenFidelity(
+    reviewIov29_31_38_40_42Explanations(IOV_EDITORIAL_BASE),
+  ),
 )
 
 export const IOV_EDITORIAL_EXPLAINED = assertCompleteOverlay(IOV_EDITORIAL_REVIEWED, 42)
